d09dbcf6eed0fcc99103e763d9f3d205cf011170
BuninaVeronika <ferenicavv@gmail.com>
2020-11-16 10:55:16 +0300

    Табуляция функции вывода значения многих переменных

--- console.c
+++ console.c
@@ -17,42 +17,36 @@ int printPtr = 0;
 
 /**
  * @brief Печатает текст в консоль \n
- * Начиная от указателя на первый символ вплоть до нуль-терминатора (\\0)
  * 2.0 Посимвольный вывод в консоль
  * 
- * @param str Указатель на первый символ текста
- * 2.0 с Указатель на переданный символ
+ * @param с переданный символ
  */
-void putchar(char *c){
+void putchar(char c){
     
   // while (*str != '\0'){
-  //Начало цикла для обработки строки
 
-        int currRow = printPtr / 2 / CONSOLE_COLS;
+  int currRow = printPtr / 2 / CONSOLE_COLS;
 
-        if (*c == '\n'){
-            int nextRow = CONSOLE_COLS * (currRow + 1);
-
-            printPtr = nextRow * 2;
+  if (c == '\n'){
+    int nextRow = CONSOLE_COLS * (currRow + 1);
+    printPtr = nextRow * 2;
             
-        } else {
-            if (printPtr/2 >= CONSOLE_ROWS * CONSOLE_COLS){
-                //printPtr = 0;
-                scrollConsole(1);
-                printPtr = (CONSOLE_ROWS-1) * CONSOLE_COLS * 2;
-            }
-            videoptr[printPtr++] = *c;
-
-        //printPtr++;
-            videoptr[printPtr++] = 0x07;
-        }
-        
-        //videoptr++;
-
-	//Код окончания цикла дл обработки строки
-	//   str++;
-	//  }
+  } else {
+    if (printPtr/2 >= CONSOLE_ROWS * CONSOLE_COLS){
+      //printPtr = 0;
+      scrollConsole(1);
+      printPtr = (CONSOLE_ROWS-1) * CONSOLE_COLS * 2;
     }
+    videoptr[printPtr++] = c;
+    //printPtr++;
+    videoptr[printPtr++] = 0x07;
+  }
+        
+  //videoptr++;
+  
+  //   str++;
+  // }
+}
 
 /**
  * @brief Очищает консоль
@@ -85,21 +79,21 @@ void scrollConsole(int n){
  */
 
 void kprint(char *str,...){
-    int i=0,u=1;
+  int i=0,u=1;
     
-     for(i=0; str[i]!='\0'; i++){
-      char symbol=str[i];
-      char next_symbol=str[i+1];
+  for(i=0; str[i]!='\0'; i++){
+    char symbol=str[i];
+    char next_symbol=str[i+1];
       
-       if(symbol=='%' && next_symbol=='i'){
-         int *param = (int *)&str;
-         char *param_int = intToStr(*(param + u));      
-          kprint(param_int);
-          i++;
-          u++;
-       }
-        else{
-            putchar(&symbol); 
+    if(symbol=='%' && next_symbol=='i'){
+      int *param = (int *)&str;
+      char *param_int = intToStr(*(param + u));      
+      kprint(param_int);
+      i++;
+      u++;
+    }
+    else{
+      putchar(symbol); 
     }
   }
 }
