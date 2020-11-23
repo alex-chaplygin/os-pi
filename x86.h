a009ec233bed3587de88ced5c8ece2a438b6e115
tsagg <mr.466@yandex.ru>
2020-11-23 17:08:45 +0300

    Добавил функции сохранения регистров в память и восстановления регистров из памяти

--- x86.h
+++ x86.h
@@ -2,5 +2,7 @@
 
 extern int read_port(int num);
 extern void write_port(int num, int data);
+extern void save_regs(int adress);
+extern void restore_regs(int adress);
 extern void load_idt(void *);
 extern ushort kernel_code;
