2ec29038a0ecf1a4e5f46d1b93ed5d46eb482b48
yri066 <yri066.ru@gmail.com>
2020-11-26 19:57:57 +0300

    Обновил файл

--- include/portable/mem.h
+++ include/portable/mem.h
@@ -1,6 +1,7 @@
 #define MAX_SEGMENTS 1024 
-#define START_ADDRESS 0x2000000 
+#define START_ADDRESS 0x200000
 #define MEM_SIZE 0x4FFFFFFF
 
 void mem_init(); 
 void *malloc(int size);
+void free(void *addr);
