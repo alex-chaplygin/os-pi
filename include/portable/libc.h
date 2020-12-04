7e01ac2c9e7e11819f3d8e1a08aea74328706e99
NikeTonik <mischenko2014@yandex.ru>
2020-12-04 20:08:06 +0300

    Изменил функции

--- include/portable/libc.h
+++ include/portable/libc.h
@@ -1,4 +1,4 @@
 void memcpy( unsigned char* destptr, unsigned char*  srcptr, int num );
-char *intToStr(int n);
-char *int_to_str_hex(int num);
-int str_to_hex(char* s);
+char *int_to_str(int n);
+char *int_to_str_hex(unsigned int num);
+unsigned int str_hex_to_int(char* s);
