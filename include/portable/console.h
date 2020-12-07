75cdecf9557c547c37436e8b77a5a32744e2bfd3
ZheniaTchernykh <zhenia.tchernykh@mail.ru>
2020-12-07 18:19:22 +0300

    Добавил сдвиг, размер буфера и функции для работы с экранами

--- include/portable/console.h
+++ include/portable/console.h
@@ -1,7 +1,17 @@
-#define CONSOLE_ROWS 25
-#define CONSOLE_COLS 80
-#define SCREEN_NUMBER 10
-#define SCREEN_BUFFER_SIZE SCREEN_NUMBER*CONSOLE_COLS*CONSOLE_ROWS*2
+#define CONSOLE_ROWS 25		/**< Количество строк на экране */
+#define CONSOLE_COLS 80		/**< Количество столбцов на экране */
+#define SCREEN_NUMBER 10	/**< Количество экранов */
+#define BUFFER_SIZE SCREEN_NUMBER*CONSOLE_COLS*CONSOLE_ROWS*2 /**< Размер буфера */
+#define SHIFT 12		/**< Строки для сдвига */
+
+/// Копирует экран из буфера в видеопамять
+void copy_to_screen();
+
+/// Переключает экран на следующий
+void screen_up();
+
+/// Переключает экран на предыдущий
+void screen_down();
 
 void putchar(char c);
 
@@ -9,4 +19,4 @@ void kprint(char *str, ...);
 
 void console_clear();
 
-void scrollConsole(int n);
+
