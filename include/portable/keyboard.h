5961f50ec47168b153e75f888bb01ade6080ecb3
zergon321 <maximgradan@gmail.com>
2020-12-10 16:11:19 +0000

    Скан-коды служебных клавиш и функция key_map добавлены.

--- include/portable/keyboard.h
+++ include/portable/keyboard.h
@@ -1,2 +1,9 @@
+#define MAX_KEYBUFFER 128
+
+#define PAGE_UP_CODE 0x49
+#define PAGE_DOWN_CODE 0x51
+#define ENTER_CODE 0x1C
+
 void init_keyboard();
 void keyboard_interrupt();
+char key_map(int scan_code);
