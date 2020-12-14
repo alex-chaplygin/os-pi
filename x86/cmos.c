185b578f07c7e9c27e64e5822c3228cdc3a12eef
NikeTonik <mischenko2014@yandex.ru>
2020-12-15 01:25:36 +0300

    Добавил функции печати времени

--- x86/cmos.c
+++ x86/cmos.c
@@ -11,6 +11,7 @@
 #include <portable/types.h>
 #include <x86/cmos.h>
 #include <x86/x86.h>
+#include <portable/libc.h>
 
 /** 
  * @brief Фунция чтения регистра cmos
@@ -35,4 +36,13 @@ void cmos_write(int num_reg, byte val)
 {
   write_port(0x70, num_reg);
   write_port(0x71, (int)val);
+}
+
+/** 
+ * @brief Печатает текущее время в формате чч:мм:сс
+ * 
+ */
+void print_time()
+{
+  kprint("Current time = %d:%d:%d", (int)cmos_read(CMOS_HOURS), (int)cmos_read(CMOS_MINUTES), (int)cmos_read(CMOS_SECONDS));
 }
