2e72f4ca1821d998c02c0894bf2b633ea605eb7f
yri066 <yri066.ru@gmail.com>
2020-12-23 09:52:34 +0300

    Добавил файл disk.c

--- x86/disk.c
+++ x86/disk.c
@@ -1,4 +1,22 @@
- int disk_read_block()
-{
+#include <x86/disk.h>
+#include <portable/types.h>
 
+byte* disk_read_block(byte *buffer, int block_num)
+{
+    byte bufData[BUFFER_SIZE];
+
+    if(block_num == 0)
+    {
+        
+    }
+    if(block_num == 1)
+    {
+        
+    }
+    if(block_num == 11)
+    {
+        
+    }
+
+    return bufData;
 }
