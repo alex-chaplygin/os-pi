c8a1a15ecd2c4d0c4154c345d81ecaecc3e13d7a
yri066 <yri066.ru@gmail.com>
2020-12-24 20:21:53 +0300

    Изменены возвращаемые данные

--- x86/disk.c
+++ x86/disk.c
@@ -5,23 +5,20 @@
  * Возвращает данные с тестового жесткого диска
  * @param buffer - буфер (размер 512), в который будут записаны данные
  * @param block_num - номер блока диска, с которого будут считываться данные
+ * @return int - 0, если всё успешно, и -1, если возникла ошибка.
  */
-void disk_read_block(byte *buffer, int block_num)
+int disk_read_block(byte *buffer, int block_num)
 {
+    if(block_num < 0)
+    {
+        return -1;
+    }
+    
     byte super_blok[] = {0x71,0x71,0x04,0x00,0x00,0x01,0x02,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00};
     byte first_blok[] = {0x66,0x69,0x6C,0x65,0x2E,0x74,0x78,0x74,0x00,0x00,0x00,0x00,0x00,0x0B,0x00,0x01};
     byte eleventh_blok[] = {0x54,0x65,0x6D,0x70,0x6F,0x72,0x61,0x72,0x79,0x20,0x63,0x6F,0x6E,0x74,0x65,0x6E,0x74,0x20,0x6F,0x66,0x20,0x66,0x69,0x6C,0x65,0x2E,0x74,0x78,0x74,0x20,0x66,0x69,0x6C,0x65};
     
-    byte bufData[BLOCK_COUNT][BLOCK_SIZE];
-
-    //Инициализация диска
-    for(int i =0; i < BLOCK_COUNT; i++)
-    {
-        for(int j = 0; j<BLOCK_SIZE;j++)
-        {
-            bufData[i][j] = 0x00;
-        }
-    }
+    byte bufData[BLOCK_COUNT][BLOCK_SIZE] = {0x00};
 
     //Заполнение суперблока
     for(int i =0; i < sizeof(super_blok); i++)
@@ -49,7 +46,7 @@ void disk_read_block(byte *buffer, int block_num)
             buffer[i] = bufData[0][i];
         }
 
-        return;
+        return 0;
     }
     if(block_num == 1)//каталок
     {
@@ -58,7 +55,7 @@ void disk_read_block(byte *buffer, int block_num)
             buffer[i] = bufData[1][i];
         }
         
-        return;
+        return 0;
     }
     if(block_num == 11)//содержимое файлаs
     {
@@ -67,7 +64,7 @@ void disk_read_block(byte *buffer, int block_num)
             buffer[i] = bufData[11][i];
         }
         
-        return;
+        return 0;
     }
 
     for(int i =0; i < BLOCK_SIZE; i++)
@@ -75,5 +72,5 @@ void disk_read_block(byte *buffer, int block_num)
         buffer[i] = 0;
     }
     
-    return;
+    return 0;
 }
