809661606ebce67b4930c50a89037d6167505421
yri066 <yri066.ru@gmail.com>
2020-12-24 15:54:39 +0300

    Изменены возвращаемые данные

--- x86/disk.c
+++ x86/disk.c
@@ -1,29 +1,79 @@
 #include <x86/disk.h>
 #include <portable/types.h>
 
-byte* disk_read_block(byte *buffer, int block_num)
+/** 
+ * Возвращает данные с тестового жесткого диска
+ * @param buffer - буфер (размер 512), в который будут записаны данные
+ * @param block_num - номер блока диска, с которого будут считываться данные
+ */
+void disk_read_block(byte *buffer, int block_num)
 {
-    byte bufData[BUFFER_SIZE];
-    byte block[BUFFER_SIZE] = "11text.txt";
-    byte path[BUFFER_SIZE] = "1text.txt";
-    byte file[BUFFER_SIZE] = "Temporary content of text.txt file";
+    byte super_blok[] = {0x71,0x71,0x04,0x00,0x00,0x01,0x02};
+    byte first_blok[] = "file.txt";
+    byte eleventh_blok[] = "Temporary content of file.txt file";
+    
+    byte bufData[BUFFER_SIZE][BLOCK_SIZE];
 
-    if(block_num == 0)
+    //Инициализация диска
+    for(int i =0; i < BUFFER_SIZE; i++)
     {
-        buffer = block;
-        return buffer;
-    }
-    if(block_num == 1)
-    {
-        buffer = path;
-        return buffer;
-    }
-    if(block_num == 11)
-    {
-        buffer = file;
-        return buffer;
+        for(int j = 0; j<BLOCK_SIZE;j++)
+        {
+            bufData[i][j] = 0x0;
+        }
     }
 
-    buffer = bufData;
+    //Заполнение суперблока
+    for(int i =0; i < 9; i++)
+    {
+        bufData[0][i] = super_blok[i];
+    }
+    
+    //Заполнение 1 блока
+    for(int i =0; i < 8; i++)
+    {
+        bufData[1][i] = first_blok[i];
+    }
+
+    //Заполнение 11 блока
+    for(int i =0; i < 34; i++)
+    {
+        bufData[11][i] = eleventh_blok[i];
+    }
+
+
+    if(block_num == 0)//суперблок
+    {
+        for(int i =0; i < BLOCK_SIZE; i++)
+        {
+            buffer[i] = bufData[0][i];
+        }
+
+        return buffer;
+    }
+    if(block_num == 1)//каталок
+    {
+        for(int i =0; i < BLOCK_SIZE; i++)
+        {
+            buffer[i] = bufData[1][i];
+        }
+        
+        return buffer;
+    }
+    if(block_num == 11)//содержимое файлаs
+    {
+        for(int i =0; i < BLOCK_SIZE; i++)
+        {
+            buffer[i] = bufData[11][i];
+        }
+        
+        return buffer;
+    }
+
+    for(int i =0; i < BLOCK_SIZE; i++)
+    {
+        buffer[i] = 0;
+    }
+    
     return buffer;
 }
