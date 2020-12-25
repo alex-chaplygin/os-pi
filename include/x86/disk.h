6c9ecbb8b3714602b5d0ccb3636571122b9d3f3f
yri066 <yri066.ru@gmail.com>
2020-12-26 02:08:31 +0300

    Добавлен заголовок функции

--- include/x86/disk.h
+++ include/x86/disk.h
@@ -1,5 +1,11 @@
 #define BLOCK_SIZE 512
 #define BLOCK_COUNT 512
+#define CATALOG_SIZE 10
+#define METADATA_SIZE 11
+#define FILE_RECORD_SIZE 16
+#define FILE_NAME_SIZE 12
 typedef unsigned char byte;
 
+void init_disk();
 int disk_read_block(byte *buffer, int block_num);
+int find_file(char *name, byte *buffer);
