2d99a3beed4cdeae466870ba73440f3a39eef178
yri066 <yri066.ru@gmail.com>
2020-12-26 08:58:02 +0300

    Добавил заголовок функции сравнения строк

--- include/portable/libc.h
+++ include/portable/libc.h
@@ -6,3 +6,4 @@ char *int_to_str_hex(unsigned int num);
 unsigned int str_hex_to_int(char* s);
 void kprint(char *str, ...);
 void clear_buffer(byte* buffer, int offset, int count);
+int str_compare(char *str_one, char *str_two);
