2132be7ec62b5dcb00d5a15244045e33d2768881
alex-chaplygin <alex_chaplygin@mail.ru>
2020-12-28 15:31:29 +0300

    Восстановление проекта

--- portable/proc.c
+++ portable/proc.c
@@ -1,5 +1,6 @@
 #include <portable/proc.h>
 #include <portable/limits.h>
+#include <portable/syscall.h>
 #include <x86/console.h>
 #include <x86/x86.h>
 
@@ -12,16 +13,18 @@ void printProc1()
   ushort *video = (ushort *)0xb8000;
   int i = 0;
   int j = 0;
+  char c = '!';
   while(1) {
+    //read_char(&c);
     i++;
     j++;
     if (i > 5) {
-      *video = 0xffaa;
+      *video = 0x0f00 + c;
       i = 0;
     }
     else *video = 0;
     if (j % 100000 == 0)
-    if (test_syscall(0, "1 ", 6) < 0) *video = 0x1111;
+    if (test_syscall(0, "123 ", 4) < 0) *video = 0x1111;
   }
 }
 
@@ -117,8 +120,10 @@ int createProc(void* codePtr, int code_size, void* dataPtr, int data_size){
     processes[freeSlot].program_counter = codePtr;
     processes[freeSlot].stack_pointer = new_stack + STACK_SIZE;
     processes[freeSlot].state = STATUS_READY;
-    processes[freeSlot].regs[54] = 0x8; // CS
-    processes[freeSlot].regs[55] = 0x200; // EFLAGS
+    processes[freeSlot].sleep_param = SLEEP_NONE;
+    for (int i = 0; i < 64; i++) processes[freeSlot].regs[i] = 0x10;//0x23; // USER DATA 
+    processes[freeSlot].regs[50] = 0x8;//0x1B; // CS
+    processes[freeSlot].regs[51] = 0x200; // EFLAGS
     return freeSlot;
 }
 
@@ -147,11 +152,30 @@ int deleteProc(unsigned int pid){
  */
 int fork()
 {
+  byte *code=(void*)malloc(current_proc->code_size);
+  byte *data=(void*)malloc(current_proc->data_size);
   // создание нового элемента в таблице процессов
+  int newproc=createProc(code,current_proc->code_size,data,current_proc->data_size);
+  if(newproc==-1){
+    return ERROR_MAXPROC;
+  }
   // установка номера родительского процесса
+  int number_parent=current_proc->pid;
   // копирование памяти для кода и данных
+  memcpy(processes[newproc].codePtr, current_proc->codePtr, current_proc->code_size);
+  memcpy(processes[newproc].dataPtr, current_proc->dataPtr, current_proc->data_size);
+
+  processes[newproc].program_counter = current_proc->program_counter;
+  processes[newproc].stack_pointer = current_proc->stack_pointer;
+  processes[newproc].state = current_proc->state;
+  processes[newproc].parent_id = number_parent;
+
+  for(int j = 0; j < REGS_SIZE-1; j++) {
+    processes[newproc].regs[j] = processes[number_parent].regs[j];
+  }
   // сохранить значение -1 в регистр eax дочернего процесса regs[REGS_SIZE - 1]
-  return 0; // возврат номера дочернего процесса или ERROR_MAXPROC
+  processes[newproc].regs[REGS_SIZE-1] = -1;
+  return newproc; // возврат номера дочернего процесса или ERROR_MAXPROC
 }
 
 /** 
@@ -231,3 +255,17 @@ void sheduler()
   restore_regs();
 }
 
+void sleep(int sleep_param) {
+  current_proc->state = STATUS_SLEEPING;
+  current_proc->sleep_param = sleep_param;
+
+  sheduler();
+}
+
+void wakeup(int sleep_param) {
+  for (int i = 0; i < MAX_PROC_AMOUNT; i++) {
+    if (processes[i].state == STATUS_SLEEPING && processes[i].sleep_param == sleep_param) {
+      processes[i].state = STATUS_READY;
+    }
+  }
+}
