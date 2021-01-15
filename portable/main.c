6bf0db8acb27c901e4373e5f323e2ccce447015f
zergon321 <maximgradan@gmail.com>
2021-01-15 11:10:25 +0000

    Проведено тестирование белого ящика функции set_attr.

--- portable/main.c
+++ portable/main.c
@@ -20,6 +20,8 @@
 #include <portable/proc.h>
 #include <x86/cmos.h>
 #include <portable/device.h>
+#include <portable/file.h>
+#include <portable/syscall.h>
 
 extern void test_syscall();
 
@@ -38,7 +40,8 @@ void kmain(void)
   init_keyboard();
   init_disk();
   init_files();
-  kprint("open file %i\n", open("file.txt"));
+  int descriptor = open("file.txt");
+  kprint("open file %i\n", descriptor);
   //kprint(intToStr(get_phys_mem_size()));
   // int a = 1 / 0;
  
@@ -56,6 +59,46 @@ void kmain(void)
   // kprint("No mouse\n");
   // } 
   // запуск процесса init
+
+  /*
+  Входные условия: id файла меньше 0.
+  Правильный класс эквивалентности: ошибка ERROR_INVALID_PARAMETERS.
+  Неправильный класс эквивалентности: возвращается 0.
+
+  Входные условия: id файла больше NUM_FILES.
+  Правильный класс эквивалентности: ошибка ERROR_INVALID_PARAMETERS.
+  Неправильный класс эквивалентности: возвращается 0.
+
+  Входные условия: по id не открыт никакой файл.
+  Правильный класс эквивалентности: ошибка ERROR_INVALID_PARAMETERS.
+  Неправильный класс эквивалентности: возвращается 0.
+
+  Входные условия: id корректен, файл открыт.
+  Правильный класс эквивалентности: возвращается 0, файлу присваиваются новые атрибуты.
+  Неправильный класс эквивалентности: ошибка ERROR_INVALID_PARAMETERS.
+
+  Входные условия: id корректен, файл открыт, вызов через sys_call.
+  Правильный класс эквивалентности: возвращается 0, файлу присваиваются новые атрибуты.
+  Неправильный класс эквивалентности: ошибка ERROR_INVALID_PARAMETERS.
+  */
+  int res = set_attr(-88, ATTR_REGULAR);
+  kprint("Test #1, expected: -4, actual: %d\n", res);
+
+  res = set_attr(322, ATTR_REGULAR);
+  kprint("Test #2, expected: -4, actual: %d\n", res);
+
+  res = set_attr(228, ATTR_REGULAR);
+  kprint("Test #3, expected: -4, actual: %d\n", res);
+
+  res = set_attr(descriptor, ATTR_DEVICE);
+  kprint("Test #4, expected: 0, actual: %d\n", res);
+  kprint("Expected attributes: 1, actual attributes: %d\n", get_attr(descriptor));
+
+  res = sys_call(5, descriptor, ATTR_DIRECTORY, 0);
+  kprint("Test #5, expected: 0, actual: %d\n", res);
+  kprint("Expected attributes: 2, actual attributes: %d\n", get_attr(descriptor));
+
+
   kprint("mem = %d\n", memory_size());
   while(1) {
     print_time();
