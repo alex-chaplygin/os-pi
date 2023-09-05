f9ef299f149d88cb67263371fcc821585cdf4a7b
Чаплыгин Александр <alex_chaplygin@mail.ru>
2023-09-05 17:47:05 +0300

    Новый формат объектов

--- portable/lisp/list.h
+++ portable/lisp/list.h
@@ -5,10 +5,10 @@
 typedef enum {
     NUMBER,
     ATOM,
-    LIST
+    PAIR
 } type_t;
 
-struct list_s;
+struct pair_s;
 
 typedef struct
 {
@@ -16,15 +16,15 @@ typedef struct
     union {
         int value; // если объект число, то его значение
         atom_t *atom; // если объект атом, то указатель на атом
-        struct list_s *list; // Если объект список то указатель на список
+        struct pair_s *pair; // Если объект пара из 2-х объектов (левый и правый) то указатель на пару
     } u;
 } object_t; // Структура объекта
 
-typedef struct list_s
+typedef struct pair_s
 {
-    object_t *elem; // элемент списка
-    struct list_s *next;// указатель на следующий элемент
-} list_t; // Структура списка
+    object_t *left; // элемент 1
+    object_t *right; // элемент 2
+} pair_t;
 
 void list_add(list_t **head, object_t *obj);
 void print_elem(object_t *head);
