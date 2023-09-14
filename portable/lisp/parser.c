b9459ab402760a21745b6eddfaef63093dee88b8
Чаплыгин Александр <alex_chaplygin@mail.ru>
2023-09-14 16:42:17 +0300

    Синтаксический разбор
    Обработка кавычки
    Обработка символов
    Обработка списка внутри списка

--- portable/lisp/parser.c
+++ portable/lisp/parser.c
@@ -28,22 +28,22 @@ char *strupr(char *str)
 }
 object_t *parse();
 
-// Обработка кавычки
-// 'a -> (quote a)
-// '(4 5 6) -> (quote (4 5 6))
-/*list_t *parse_quote()
+/**
+ *  Обработка кавычки
+ *  'a -> (quote a)
+ * '(4 5 6) -> (quote (4 5 6))
+ *
+ * @return указатель на список с quote
+ */
+object_t *parse_quote()
 {
-    list_t *list = NULL;
     object_t *o = parse();
+    object_t *p = new_pair(o, NULL);
     if (o->type == NUMBER)
-        error("quote Number\n");
-    else 
-    {
-        list_add(&list, object_new(ATOM, "QUOTE"));
-        list_add(&list, o);
-    }
-    return list;
-    }*/
+	error("quote Number\n");
+    else
+	return new_pair(object_new(SYMBOL, "QUOTE"), p);
+}
 
 /** 
  * Обработка списка без левой скобки
@@ -52,15 +52,20 @@ object_t *parse();
  * @return указатель на объект списка
  */
 object_t *parse_list()
-{
-  int val;
-  token_t *cur_token = get_token();
-  if (cur_token->type == RPAREN)
-    return NULL;
-  if (cur_token->type == T_NUMBER)
-    return new_pair(object_new(NUMBER, &cur_token->value), parse_list());
-    /*    else if (cur_token->type == T_ATOM)
-      list_add(&list, object_new(ATOM, strupr(cur_token->str)));
+    {
+      int val;
+      token_t *cur_token = get_token();
+      if (cur_token->type == RPAREN)
+	return NULL;
+      if (cur_token->type == T_NUMBER)
+	return new_pair(object_new(NUMBER, &cur_token->value), parse_list());
+      else if (cur_token->type == T_SYMBOL)
+	return new_pair(object_new(SYMBOL, cur_token->str), parse_list());
+      else if (cur_token->type == LPAREN){
+	object_t *list = parse_list();
+	return new_pair(list, parse_list());
+      }
+      /* 
     else if  (cur_token->type == LPAREN)
       list_add(&list, object_new(LIST, parse_list()));
     else if (cur_token->type == QUOTE)
@@ -73,21 +78,23 @@ object_t *parse_list()
 }
 
 
-
-// Обработка выражения 
-// (1 2)
-// 12
-// ABC
-/*object_t *parse()
+/**
+ * Читает выражение и строит обьект этого выражения
+ * (1 2)
+ * 12
+ * ABC
+ * @return указатель на объект
+ */
+object_t *parse()
 {   
     object_t *el; // создаем новый элемент
     cur_token = get_token(); // считывается левая скобка
     if (cur_token->type == T_NUMBER) // считывается число
-        return object_new(NUMBER, &cur_token->value);
-    else if (cur_token->type == T_ATOM)//считывается атом
-        return object_new(SYMBOL, find_symbol(strupr(cur_token->str)));
+	return object_new(NUMBER, &cur_token->value);
+    else if (cur_token->type == T_SYMBOL)//считывается атом
+	return object_new(SYMBOL, find_symbol(strupr(cur_token->str)));
     else if (cur_token->type == LPAREN)
-      return parse_list();
+	return parse_list();
     else if (cur_token->type == QUOTE)
-        return object_new(LIST, parse_quote());
-	}*/
+	return parse_quote();
+}
