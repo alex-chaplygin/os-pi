c38a9a2754d6a5c9ca0395e71dc15e896712f201
Чаплыгин Александр <alex_chaplygin@mail.ru>
2023-09-14 16:39:19 +0300

    Преобразование вычислений
    Переименование функций
    Регистрация примитивов
    Функция quote
    Вычисление через значение примитива

--- portable/lisp/eval.c
+++ portable/lisp/eval.c
@@ -22,7 +22,7 @@ object_t *nil;
  * 
  * @return указатель на значиение первого элемента списка
  */
-object_t *CAR(object_t *list)
+object_t *car(object_t *list)
 {
     if (list->type != PAIR)
         error("Not list in car\n");
@@ -37,7 +37,7 @@ object_t *CAR(object_t *list)
  * 
  * @return указатель на второй элемент списка
  */
-object_t *CDR(object_t *list)
+object_t *cdr(object_t *list)
 {
     if (list->type != PAIR)
         error("Not list in cdr\n");
@@ -53,7 +53,7 @@ object_t *CDR(object_t *list)
  *
  * @return t или nil
  */
-object_t *eval_eq(object_t *list)
+object_t *eq(object_t *list)
 {
     object_t *p1 = FIRST(list);
     object_t *p2 = SECOND(list);
@@ -67,6 +67,18 @@ object_t *eval_eq(object_t *list)
         return nil;
 }
 
+/** 
+ * возвращает свой аргумент без вычисления
+ *
+ * @param list - список параметров (1 параметр)
+ *
+ * @return аргумент
+ */
+object_t *quote(object_t *list)
+{
+    return FIRST(list);
+}
+
 //
 /** 
  * Конструирование объекта ожидает, что 2 параметр - список
@@ -75,7 +87,7 @@ object_t *eval_eq(object_t *list)
  *
  * @return список который содержит 1 параметр, и продолжается с элементами 2 параметра.
  */
-object_t *CONS(object_t *list)
+object_t *cons(object_t *list)
 {			
   /*    if (list->type != PAIR)
         error("Not list in cons\n");
@@ -85,44 +97,40 @@ object_t *CONS(object_t *list)
         error("second parameter not list");
 	return new_pair(p1, p2);*/
 }
-      
-//Вычисление выражения
-//Если выражение число, возвращаем его же
-//Если выражение атом, то выдаем ошибку
-//Если список, получаем первый элемент списка и обрабатываем эту функцию
-// (quote a) -> a
-// (eq 'a 'a) -> T
-// (eq 'a 'b) -> ()
-// (car '(1 2 3)) -> 1
-// (cdr '(1 2 3)) -> (2 3)
-//obj - входное выражение
-//возвращает вычисленный объект
-/*object_t *eval(object_t *obj)
+
+/**
+ * Вычисление выражения
+ * Если выражение число, возвращаем его же
+ * Если выражение атом, то выдаем ошибку
+ * Если список, получаем первый элемент списка и обрабатываем эту функцию
+ *  (quote a) -> a
+ *  (eq 'a 'a) -> T
+ *  (eq 'a 'b) -> ()
+ *  (car '(1 2 3)) -> 1
+ *  (cdr '(1 2 3)) -> (2 3)
+ * @param obj входное выражение
+ * @return возвращает вычисленный объект
+ */
+object_t *eval(object_t *obj)
 {
     if (obj->type == NUMBER)
         return obj;
-    else if (obj->type == ATOM)
-        error("Unknown ATOM \n");
-    else if (CAR(obj)->u.atom == quote)
-        return CDAR(obj);
-    else if (CAR(obj)->u.atom == eq)
-        return eval_eq(eval(CDAR(obj)), eval(CDDAR(obj)));
-    else if (CAR(obj)->u.atom == car)
-        return CAR(eval(CDAR(obj)));
-    else if (CAR(obj)->u.atom == cdr)
-        return CDR(eval(CDAR(obj)));    
+    else if (obj->type == SYMBOL)
+        error("Unknown SYMBOL \n");
+    else if (obj->type == PAIR) {
+      symbol_t *s = find_symbol(obj->u.symbol->str);
+      return s->func(obj->u.pair->right);
+    }
     else 
         error("Unknown func\n");
-	}*/
+}
 
 //инициализация примитивов
- /*void init_eval()
+void init_eval()
 {
-  register_symbol("CAR", CAR);
-    car = find_atom("CAR");
-    cdr = find_atom("CDR");
-    quote = find_atom("QUOTE");
-    eq = find_atom("EQ");
-    t = object_new(ATOM, "T");
-    nil = object_new(PAIR, 0);
-    }*/
+  register_func("CAR", car);
+  register_func("CDR", cdr);    
+  register_func("EQ", eq);
+  t = object_new(SYMBOL, "T");
+  nil = NULL;
+}
