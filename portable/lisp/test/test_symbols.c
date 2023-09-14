8682a5c29b7198b962be3a4de39ddfa0e5567852
Чаплыгин Александр <alex_chaplygin@mail.ru>
2023-09-14 16:44:36 +0300

    Тестирование регистрации функций

--- portable/lisp/test/test_symbols.c
+++ portable/lisp/test/test_symbols.c
@@ -29,10 +29,26 @@ void test_find_same_symbol()
    ASSERT(i, k)
 }
 
+object_t *test(object_t *list)
+{
+  return NULL;
+}
+
+/**
+ * зарегистрировать функцию и проверить указатель
+ */
+void test_register_func()
+{
+  printf("test_register_func: ");
+  register_func("TEST", test);
+  ASSERT(find_symbol("TEST")->func, test); 
+}
+
 int main()
 {
   test_str_copy();
   test_compare_str("abc", "abc", 1);
   test_compare_str("abc", "abc1", 0);
   test_find_same_symbol();
+  test_register_func();
 }
