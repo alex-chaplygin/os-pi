1ed68d82df4b6c9d3e70a0e5a1a7922d412851f4
Чаплыгин Александр <alex_chaplygin@mail.ru>
2023-09-15 18:01:37 +0300

    Интеграционный тест CAR и CONS

--- portable/lisp/test/test_eval_int.c
+++ portable/lisp/test/test_eval_int.c
@@ -1,25 +1,55 @@
 #include <stdio.h>
+#include "objects.h"
 #include "eval.h"
 #include "test.h"
 
 /**
- * создать объект для выражения (car (quote 5))
+ * создать объект для выражения (car (quote (5)))
  * вычислить объект 
  */
 void test_car()
 {
     printf("test_car: ");
-  
-    object_t num = {NUMBER, 5};  
-    pair_t node = {&num, NULL};
-    object_t lst = {PAIR, &node};
-    
+
+    int e = 5;
+    object_t *l = new_pair(object_new(NUMBER, &e), NULL);
+    object_t *q = new_pair(object_new(SYMBOL, "QUOTE"),
+			   new_pair(l, NULL));
+    object_t *o = new_pair(object_new(SYMBOL, "CAR"),
+			   new_pair(q, NULL));
+    object_t *res = eval(o);
+    ASSERT(res->type, NUMBER);
     ASSERT(res->u.value, 5);
+}
+
+/**
+ * создать объект для выражения (cons (quote a) (quote (5)))
+ * вычислить объект (A 5)
+ */
+
+void test_cons()
+{
+    printf("test_cons: ");
+    int e = 5;
+    object_t *l = new_pair(object_new(NUMBER, &e), NULL);
+    object_t *q = new_pair(object_new(SYMBOL, "QUOTE"),
+			   new_pair(l, NULL));
+    object_t *qa = new_pair(object_new(SYMBOL, "QUOTE"),
+			    new_pair(object_new(SYMBOL, "A"), NULL));
+    object_t *o = new_pair(object_new(SYMBOL, "CONS"),
+			    new_pair(qa, new_pair(q, NULL)));
+    object_t *res = eval(o);
+    ASSERT(FIRST(res)->type, SYMBOL);
+    ASSERT(SECOND(res)->type, NUMBER);
+    ASSERT(SECOND(res)->u.value, 5);
 }
 
 int main()
 {
+    printf("------------test_eval_int---------\n");
     init_eval();
+    test_car();
+    test_cons();
     return 0;
 }
 
