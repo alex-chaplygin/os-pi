a9acb516a610dae47eddf39fd28c646742485b1b
Чаплыгин Александр <alex_chaplygin@mail.ru>
2023-09-25 07:31:21 +0300

    Исправлены ошибки в тестах

--- portable/lisp/test/test_eval.c
+++ portable/lisp/test/test_eval.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include "symbols.h"
 #include "test.h"
+#include "parser.h"
 
 extern object_t *t;
+object_t test_res;
+pair_t test_pair;
 
 void print_obj(object_t *head){}
 
@@ -12,10 +15,16 @@ object_t *object_new(type_t type, void *data){
 
 object_t *new_pair(object_t *left, object_t *right)
 {
-    return NULL;
+    test_pair.left = left;
+    test_pair.right = right->u.pair->left;
+    test_res.type = PAIR;
+    test_res.u.pair = &test_pair;
+    return &test_res;
 }
+
 object_t *car(object_t *list);
 object_t *cdr(object_t *list);
+object_t *cons(object_t *list);
 object_t *eq(object_t *list);
 object_t *atom(object_t *obj);
 object_t *quote(object_t *list);
@@ -58,7 +67,7 @@ void test_eq_num()
     ob1.type = PAIR;
     ob1.u.pair = &p1;
     
-    ASSERT(eq(&ob1), NULL);
+    ASSERT(eq(&ob1), ERROR);
 }
 
 //символы не равны
@@ -124,21 +133,30 @@ void test_eq_sym_eq()
 void test_cdr()
 {
     printf("test_cdr: ");
-  
+
     object_t num;
     num.type = NUMBER;
     num.u.value = 5;
     
+    pair_t node1;
+    node1.left = &num;
+    node1.right = &num;
+    
+    object_t pair;
+    pair.type = PAIR;
+    pair.u.pair = &node1;
+    
     struct pair_s node;
-    node.left = &num;
-    node.right = NULL;
+    node.left = &pair;
+    node.right = &num;
     
     object_t lst;
     lst.type = PAIR;
     lst.u.pair = &node;
     
     object_t* res = cdr(&lst);
-    ASSERT(res, NULL);
+    
+    ASSERT(res->u.value, 5);
 }
 
 void test_car()
@@ -168,14 +186,35 @@ void test_car()
 void test_cons()
 {
     printf("test_cons: ");
-    object_t num1, num2;
+    object_t num1, num2, obj_p1, obj_p2, obj_p3;
     pair_t p1, p2, p3;
         
     num1.type = NUMBER;
     num1.u.value = 1;
     num2.type = NUMBER;
-    num2.u.value = 2;    
-    //object_t* res = CONS()
+    num2.u.value = 2;
+    
+    p1.left = &num2;
+    p1.right = NULL;
+    obj_p1.type = PAIR;
+    obj_p1.u.pair = &p1;
+    
+    p2.left = &obj_p1;
+    p2.right = NULL;
+    obj_p2.type = PAIR;
+    obj_p2.u.pair = &p2;
+    
+    p3.left = &num1;
+    p3.right = &obj_p2;
+    
+    obj_p3.type = PAIR;
+    obj_p3.u.pair = &p3;
+    
+    object_t *res = cons(&obj_p3);
+
+    ASSERT(res->type, PAIR);
+    ASSERT(res->u.pair->left->u.value, 1);
+    ASSERT(res->u.pair->right->u.value, 2);
 }
 
 // тип объекта - атом
@@ -240,6 +279,7 @@ int main()
     test_eq_num();
     test_eq_sym();
     test_eq_sym_eq();
+    test_cons();
     test_atom_is();
     test_atom_not();
     test_quote();
