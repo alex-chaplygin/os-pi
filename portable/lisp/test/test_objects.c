b01f62a8dc0ad130f1e54e80c73115fe27f1c3f5
Чаплыгин Александр <alex_chaplygin@mail.ru>
2023-09-29 19:23:20 +0300

    Оформление теста

--- portable/lisp/test/test_objects.c
+++ portable/lisp/test/test_objects.c
@@ -9,7 +9,7 @@ extern int last_object;
 
 symbol_t *find_symbol(char *str)
 {
-  return NULL;
+    return NULL;
 }
 
 /** 
@@ -17,13 +17,13 @@ symbol_t *find_symbol(char *str)
  */
 void test_object_new()
 {
-  int number = 10;
-  printf("test_object_new: ");
-  object_new(NUMBER, &number);
-  number = 11;
-  object_new(NUMBER, &number);
-  ASSERT(objects[0].u.value, 10);
-  ASSERT(objects[1].u.value, 11);
+    int number = 10;
+    printf("test_object_new: ");
+    object_new(NUMBER, &number);
+    number = 11;
+    object_new(NUMBER, &number);
+    ASSERT(objects[0].u.value, 10);
+    ASSERT(objects[1].u.value, 11);
 }
 
 /** 
@@ -31,17 +31,17 @@ void test_object_new()
  */
 void test_new_pair()
 {
-  object_t o1, o2, o3, o4;
-  o1.type = NUMBER;
-  o1.u.value = 5;
-  printf("test_new_pair: ");
-  new_pair(&o1, &o2);
-  new_pair(&o3, &o4);
-  ASSERT(pairs[0].left, &o1);
-  ASSERT(pairs[0].left->u.value, 5);
-  ASSERT(pairs[0].right, &o2);
-  ASSERT(pairs[1].left, &o3);
-  ASSERT(pairs[1].right, &o4);
+    object_t o1, o2, o3, o4;
+    o1.type = NUMBER;
+    o1.u.value = 5;
+    printf("test_new_pair: ");
+    new_pair(&o1, &o2);
+    new_pair(&o3, &o4);
+    ASSERT(pairs[0].left, &o1);
+    ASSERT(pairs[0].left->u.value, 5);
+    ASSERT(pairs[0].right, &o2);
+    ASSERT(pairs[1].left, &o3);
+    ASSERT(pairs[1].right, &o4);
 }
 
 /** 
@@ -49,16 +49,16 @@ void test_new_pair()
  */
 void test_print_obj()
 {
-  object_t *ob;
+    object_t *ob;
   
-  printf("test_print_obj: ");
+    printf("test_print_obj: ");
   
-  int num1 = 1;
-  int num2 = 2;
+    int num1 = 1;
+    int num2 = 2;
   
-  ob = new_pair(object_new(NUMBER, &num1), new_pair(object_new(NUMBER, &num2), NULL));
-  print_obj(ob);
-  printf("\n");
+    ob = new_pair(object_new(NUMBER, &num1), new_pair(object_new(NUMBER, &num2), NULL));
+    print_obj(ob);
+    printf("\n");
 }
 
 /**
@@ -66,28 +66,29 @@ void test_print_obj()
  *  объектов и их дальнейшего переиспользования
  */
 void test_free(){
-  printf("test_free_object: ");
-  int last_num = last_object;
-  for (int i = last_object; 100 > i; i++)
-    object_new(NUMBER, &i);
-  object_t *o1 = &objects[0];
-  object_t *o2 = &objects[5];
-  free_object(o1);
-  free_object(o2);
-  ASSERT(free_objs, o2);
-  ASSERT(free_objs->next, o1);
-  ASSERT(free_objs->next->next, NULL);
-  object_t *r_o1 = object_new(SYMBOL, "1");
-  object_t *r_o2 = object_new(SYMBOL, "2");
-  ASSERT(o2, r_o1);
-  ASSERT(o1, r_o2);
-  ASSERT(free_objs, NULL);
+    printf("test_free_object: ");
+    int last_num = last_object;
+    for (int i = last_object; 100 > i; i++)
+	object_new(NUMBER, &i);
+    object_t *o1 = &objects[0];
+    object_t *o2 = &objects[5];
+    free_object(o1);
+    free_object(o2);
+    ASSERT(free_objs, o2);
+    ASSERT(free_objs->next, o1);
+    ASSERT(free_objs->next->next, NULL);
+    object_t *r_o1 = object_new(SYMBOL, "1");
+    object_t *r_o2 = object_new(SYMBOL, "2");
+    ASSERT(o2, r_o1);
+    ASSERT(o1, r_o2);
+    ASSERT(free_objs, NULL);
 }
 
 void main()
 {
-  test_object_new();
-  test_new_pair();
-  test_print_obj();
-  test_free();
+    printf("--------------test objects---------------------\n");
+    test_object_new();
+    test_new_pair();
+    test_print_obj();
+    test_free();
 }
