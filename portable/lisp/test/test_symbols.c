05a0a9386e0b1957f4a453b1aecb03944872f91b
Чаплыгин Александр <alex_chaplygin@mail.ru>
2023-09-29 19:24:39 +0300

    Оформление теста

--- portable/lisp/test/test_symbols.c
+++ portable/lisp/test/test_symbols.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 #include "test.h"
+#include "objects.h"
 #include "symbols.h"
 
 void str_copy (char *str1, char *str2);
 int compare_str(char *str1, char *str2);
 
+symbol_t s;
+
+symbol_t *new_symbol(char *str)
+{
+    return &s;
+}
 
 void test_str_copy()
 {
@@ -12,26 +19,26 @@ void test_str_copy()
     char b[] = "sdadas";
     printf("test_str_copy");
     str_copy(a, b);
-    ASSERT(1, compare_str("fdfd", a))
+    ASSERT(1, compare_str("fdfd", a));
 }
 
 void test_compare_str(char *str, char *str2, int res)
 {
-  printf("test_compare_str: ");
-  ASSERT(res, compare_str(str, str2));
+    printf("test_compare_str: ");
+    ASSERT(res, compare_str(str, str2));
 }
 
 void test_find_same_symbol()
 {
-   symbol_t *i = find_symbol("f");
-   symbol_t *k = find_symbol("f");
-   printf("test_find_same_symbol: ");
-   ASSERT(i, k)
+    symbol_t *i = find_symbol("f");
+    symbol_t *k = find_symbol("f");
+    printf("test_find_same_symbol: ");
+    ASSERT(i, k);
 }
 
 object_t *test(object_t *list)
 {
-  return NULL;
+    return NULL;
 }
 
 /**
@@ -39,16 +46,17 @@ object_t *test(object_t *list)
  */
 void test_register_func()
 {
-  printf("test_register_func: ");
-  register_func("TEST", test);
-  ASSERT(find_symbol("TEST")->func, test); 
+    printf("test_register_func: ");
+    register_func("TEST", test);
+    ASSERT(find_symbol("TEST")->func, test); 
 }
 
 int main()
 {
-  test_str_copy();
-  test_compare_str("abc", "abc", 1);
-  test_compare_str("abc", "abc1", 0);
-  test_find_same_symbol();
-  test_register_func();
+    printf("--------------test symbols---------------------\n");
+    test_str_copy();
+    test_compare_str("abc", "abc", 1);
+    test_compare_str("abc", "abc1", 0);
+    test_find_same_symbol();
+    test_register_func();
 }
