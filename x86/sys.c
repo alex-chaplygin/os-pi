5d4dcb90c99292b99503788214d1cd3dc2eb91c7
Чаплыгин Александр <alex_chaplygin@mail.ru>
2023-11-05 16:06:36 +0300

    Чтение и запись массивов

--- x86/sys.c
+++ x86/sys.c
@@ -8,6 +8,7 @@
  */
 
 #include <x86/x86.h>
+#include <portable/libc.h>
 #include <objects.h>
 #include <symbols.h>
 #include <eval.h>
@@ -117,6 +118,61 @@ object_t *OUTDW(object_t *args)
     return NULL;
 }
 
+/**
+ * Чтение массива слов из порта
+ *
+ * @param args (<номер порта> <число слов>)
+ *
+ * @return объект массив
+ */
+object_t *INSW(object_t *args)
+{
+    if (args == NULL || args->type != PAIR) {
+	error("INSW: port\n");
+	return ERROR;
+    }
+    int port = FIRST(args)->u.value;
+    int size = SECOND(args)->u.value;
+    byte *buf = alloc_region(size * sizeof(short));
+    inw_arr(port, size, buf);
+    size <<= 1;
+    array_t *a = new_empty_array(size);
+    byte *b = buf;
+    int val;
+    for (int i = 0; i < size; i++) {
+	val = *b++;
+	a->data[i] = object_new(NUMBER, &val);
+    }
+    free_region(buf);
+    return object_new(ARRAY, a);
+}
+
+/** 
+ * Запись массива байт (словами) в порт
+ *
+ * @param args (номер_порта массив)
+ *
+ * @return nil
+ */
+object_t *OUTSW(object_t *args)
+{
+    if (args == NULL || args->type != PAIR || args->u.pair->right == NULL) {
+	error("OUTSW: port val\n");
+	return ERROR;
+    }
+    int port = FIRST(args)->u.value;
+    array_t *a = SECOND(args)->u.arr;
+    byte *buf = alloc_region(a->length);
+    byte *dst = buf;
+    object_t **src = a->data;
+    for (int i = 0; i < a->length; i++) {
+	*dst++ = (*src)->u.value;
+	src++;
+    }
+    outw_arr(port, a->length >> 1, buf);
+    return NULL;
+}
+
 /** 
  * Регистрация системных функций
  */
@@ -128,4 +184,6 @@ void init_sys()
     register_func("OUTW", OUTW);
     register_func("INDW", INDW);
     register_func("OUTDW", OUTDW);
+    register_func("INSW", INSW);
+    register_func("OUTSW", OUTSW);
 }
