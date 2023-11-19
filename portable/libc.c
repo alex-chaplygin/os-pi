476bd8dbc508675424e118ce205bd4c8ec6e33d4
Чаплыгин Александр <alex_chaplygin@mail.ru>
2023-11-19 10:41:44 +0300

    Исправлена ошибка при конкатенации строк

--- portable/libc.c
+++ portable/libc.c
@@ -264,7 +264,8 @@ int strlen(char *str)
  */
 void strcat(char *str1, char *str2)
 {
-    while (*str1++ != 0);
+    while (*str1 != 0)
+	str1++;
     while (*str2 != 0)
 	*str1++ = *str2++;
     *str1 = 0;
