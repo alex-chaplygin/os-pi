b62d3dd181a3164a0e010dfae25f78c94c179cc8
Чаплыгин Александр <alex_chaplygin@mail.ru>
2023-12-23 15:54:05 +0300

    Добавлены новые тесты

--- portable/lisp/test/test_objects.c
+++ portable/lisp/test/test_objects.c
@@ -126,97 +126,45 @@ void reset_mem()
     free_strings = NULL;
 }
 
-/**
- * Проверка корректности функции освобождения
- *  объектов и их дальнейшего переиспользования
- */
-void test_free_object()
-{
-    printf("test_free_object: ");
-    reset_mem();
-    int last_num = last_object;
-    for (int i = last_object; i < MAX_OBJECTS; i++)
-	object_new(NUMBER, &i);
-    object_t *o1 = &objects[0];
-    object_t *o2 = &objects[5];
-    free_object(o1);
-    free_object(o2);
-    ASSERT(free_objs, o2);
-    ASSERT(free_objs->next, o1);
-    ASSERT(free_objs->next->next, NULL);
-    object_t *r_o1 = object_new(SYMBOL, "1");
-    object_t *r_o2 = object_new(SYMBOL, "2");
-    ASSERT(o2, r_o1);
-    ASSERT(o1, r_o2);
-    ASSERT(free_objs, NULL);
-}
+/* /\** */
+/*  * Проверка корректности функции освобождения */
+/*  *  объектов и их дальнейшего переиспользования */
+/*  *\/ */
+/* void test_free_object() */
+/* { */
+/*     printf("test_free_object: "); */
+/*     reset_mem(); */
+/*     int last_num = last_object; */
+/*     for (int i = last_object; i < MAX_OBJECTS; i++) */
+/* 	object_new(NUMBER, &i); */
+/*     object_t *o1 = &objects[0]; */
+/*     object_t *o2 = &objects[5]; */
+/*     free_object(o1); */
+/*     free_object(o2); */
+/*     ASSERT(free_objs, o2); */
+/*     ASSERT(free_objs->next, o1); */
+/*     ASSERT(free_objs->next->next, NULL); */
+/*     object_t *r_o1 = object_new(SYMBOL, "1"); */
+/*     object_t *r_o2 = object_new(SYMBOL, "2"); */
+/*     ASSERT(o2, r_o1); */
+/*     ASSERT(o1, r_o2); */
+/*     ASSERT(free_objs, NULL); */
+/* } */
 
-/** 
- * Проверка на получение объекта, равного NULL
- */
-void test_free_object_null()
-{
-    printf("test_free_object_null: ");
-    reset_mem();
-    int n = 5;
-    object_t *o3 = NULL;
-    object_t *o4 = object_new(NUMBER, &n);    
-    free_object(o4);
-    free_object(o3);    
-    ASSERT(free_objs, o4);
-}
-
-/**
- * Проверка корректности функции освобождения пары
- *  объектов и их дальнейшего переиспользования
- */
-void test_free_pair()
-{
-    printf("test_free_pair: ");
-    reset_mem();
-    int last_num = last_pair;
-    for (int i = last_object; i < MAX_PAIRS; i++)
-    {   
-        object_t o1, o2;
-        o1.type = NUMBER;
-        o1.u.value = i;
-        o2.type = NUMBER;
-        o2.u.value = i-1;
-	new_pair(&o1, &o2);
-    }
-    pair_t *pair1 = &pairs[0];
-    pair_t *pair2 = &pairs[1];
-    free_pair(pair1);
-    free_pair(pair2);
-    ASSERT(free_pairs, pair2);
-    ASSERT(free_pairs->next, pair1);
-    ASSERT(free_pairs->next->next, NULL);
-}
-
-/**
- * Создать объект (12 A 5)
- */
-void test_mark()
-{
-    printf("test_mark :");
-    reset_mem();
-    print_free_objs();
-    int n1 = 12;
-    int n2 = 5;
-    object_t *num1 = object_new(NUMBER, &n1);
-    object_t *sym = object_new(SYMBOL, "A");
-    object_t *num2 = object_new(NUMBER, &n2);
-    object_t *p3 = new_pair(num2, NULL);
-    object_t *p2 = new_pair(sym, p3);
-    mobject = new_pair(num1, p2);
-    mark_object(mobject);
-    ASSERT(mobject->mark, 1);
-    ASSERT(num1->mark, 1);
-    ASSERT(p2->mark, 1);
-    ASSERT(sym->mark, 1);
-    ASSERT(p3->mark, 1);
-    ASSERT(num2->mark, 1);
-}
+/* /\**  */
+/*  * Проверка на получение объекта, равного NULL */
+/*  *\/ */
+/* void test_free_object_null() */
+/* { */
+/*     printf("test_free_object_null: "); */
+/*     reset_mem(); */
+/*     int n = 5; */
+/*     object_t *o3 = NULL; */
+/*     object_t *o4 = object_new(NUMBER, &n);     */
+/*     free_object(o4); */
+/*     free_object(o3);     */
+/*     ASSERT(free_objs, o4); */
+/* } */
 
 /** 
  *Освобождение большого числа из объекта
@@ -257,204 +205,144 @@ void test_free_pair()
 }
 
 /**
- * Используем помеченные объекты с предыдущего теста.
- * Проверить снятие пометок
- * Проверить, что помеченные ранее объекты не находятся в списке свободных
+ * Создание нового символа и проверка его
  */
-void test_sweep()
+ void test_new_symbol() 
 {
-    printf("test_sweep: ");
-    print_free_objs();
-    object_t *num1 = mobject->u.pair->left;
-    object_t *p2 = mobject->u.pair->right;
-    object_t *sym = p2->u.pair->left;
-    object_t *p3 = p2->u.pair->right;
-    object_t *num2 = p3->u.pair->left;
-    sweep();
-    printf("after_sweep: ");
-    print_free_objs();
-    ASSERT(mobject->mark, 0);
-    ASSERT(num1->mark, 0);
-    ASSERT(p2->mark, 0);
-    ASSERT(sym->mark, 0);
-    ASSERT(p3->mark, 0);
-    ASSERT(num2->mark, 0); 
-    object_t *f = free_objs;
-    while (f != NULL) {
-	//	printf("f type =%d\n", f->type);
-	if (f == mobject || f == num1 || f == p2 || f == sym || f == p3 || f == num2) {
-	    printf("fail_object\n");
-	    return;
-	}
-	f = f->next;
-    }
-    printf("objects: OK\n");
+    printf("test_new_symbol: ");
+    symbol_t *s = new_symbol("abc");
+    ASSERT(strcmp(s->str, "abc"), 0);
 }
 
-/**
- * Создать символ A
- * Присвоить ему значение - объект 52
- * Проверить, что объект не в списке свободных
- */
-void test_garbage_collect()
-{
-    printf("test_garbage_collect: ");
-    int num1 = 52;
-    symbol_t *s = new_symbol("A");
-    object_t *obj1 = object_new(NUMBER, &num1);
-    s->value = obj1;
-    garbage_collect();
-
-    object_t *f = free_objs;
-    while (f != NULL) {
-	if (f == obj1) {
-	    printf("fail_object\n");
-	    return;
-	}
-	f = f->next;
-    }
-    printf("OK\n");
-}
-
-/**
- * Создать символ B
- * Присвоить ему значение - объект (1 2)
- * Проверить, что объект не в списке свободных
- */
-void test_garbage_collect_list()
-{
-    printf("test_garbage_collect_list: ");
-    int num1 = 1;
-    int num2 = 2;
-    symbol_t *s = new_symbol("B");
-    object_t *obj1 = object_new(NUMBER, &num1);
-    object_t *obj2 = object_new(NUMBER, &num2);
-    object_t *p1 = new_pair(obj1, new_pair(obj2, NULL));
-    s->value = p1;
-    printf("before gc: \n");
-    printf("objects: ");
-    print_free_objs();
-    printf("\npairs: ");
-    print_free_pairs();
-    garbage_collect();
-    printf("after gc: ");
-    print_free_objs();
-
-    object_t *f = free_objs;
-    while (f != NULL) {
-	if (f == obj1 || f == obj2) {
-	    printf("fail_object\n");
-	    return;
-	}
-	f = f->next;
-    }
-    printf("objects_OK\n");
-    pair_t *p = free_pairs;
-    while (p != NULL) {
-	if (p == p1->u.pair || p == p1->u.pair->right->u.pair) {
-	    printf("fail_pair\n");
-	    return;
-	}
-	p = p->next;
-    } 
-    printf("pairs: OK\n");
-}
-
-/**
- * Создать две пары
- * Присвоить левым частям этих пар - числа, а правые ссылались друг на друга
- * Создать два символа, которые ссылаются на эти пары
- * Проверить работу сборщика мусора, у пар поле free должно быть равно нулю.
- */
-void test_garbage_collect_cycle()
-{
-    printf("test_garbage_collect_cycle: ");
-    int num1 = 1;
-    symbol_t *s1 = new_symbol("A");
-    symbol_t *s2 = new_symbol("B");
-    object_t *p1 = new_pair(object_new(NUMBER, &num1), NULL);
-    object_t *p2 = new_pair(object_new(NUMBER, &num1), NULL);
-    p1->u.pair->right = p2;
-    p2->u.pair->right = p1;
-    s1->value = p1;
-    s2->value = p2;
-    printf("p1 = ");
-    PRINT(p1);
-    garbage_collect();
-
-    ASSERT(p1->free, 0);
-    ASSERT(p2->free, 0);
-}
-
-/**
- * Выделить 2 региона с размерами 5 и 64
- * Проверить указатели регионов относительно начала
- * Проверить указатели данных
- */
-void test_alloc_region()
-{   
-    printf("test_alloc_region: ");
-    char *reg1 = alloc_region(5);
-    char *reg2 = alloc_region(64);
-    int size = 3 * sizeof(int) + 2 * sizeof(int *);
-    ASSERT((reg1 - region_data), size);
-    ASSERT((reg2 - region_data), size + 8 + size);
-    //    free_region(reg1);
-    //free_region(reg2);
-}
-
-/**
- * Создать 3 региона
- * Удалить второй и первый регион 
- * Проверить объединённый регион
- * Освободить третий регион
- */
-void test_free_region()
-{   
-    printf("test_free_region: ");
-    char *reg1 = alloc_region(8);
-    char *reg2 = alloc_region(12);
-    char *reg3 = alloc_region(16);
-    char *reg4 = alloc_region(10);
-    int offset = 3 * sizeof(int) + 2 * sizeof(int *);
-    struct region *r1 = (struct region *)(reg1 - offset);
-    struct region *r2 = (struct region *)(reg2 - offset);
-    struct region *r3 = (struct region *)(reg3 - offset);
-    free_region(reg2);
-    ASSERT(r1->next, r2);
-    ASSERT(r2->prev, r1);
-    free_region(reg1);
-    ASSERT(r1->next, r3);
-    ASSERT(r3->prev, r1);
-    ASSERT(r1->size, 20 + offset);
-    free_region(reg3);
-    ASSERT(r1->size, 36 + 2 * offset);
-}
-
-/**
-/*  * Создать две пары */
-/*  * Присвоить левым частям этих пар - числа, а правые ссылались друг на друга */
-/*  * Создать два символа, которые ссылаются на эти пары */
-/*  * Проверить работу сборщика мусора, у пар поле free должно быть равно нулю. */
+/* /\** */
+/*  * Создать объект (12 A 5) */
 /*  *\/ */
-/* void test_garbage_collect_cycle() */
+/* void test_mark() */
 /* { */
-/*     printf("test_garbage_collect_cycle: "); */
-/*     int num1 = 1; */
-/*     symbol_t *s1 = new_symbol("A"); */
-/*     symbol_t *s2 = new_symbol("B"); */
-/*     object_t *p1 = new_pair(object_new(NUMBER, &num1), NULL); */
-/*     object_t *p2 = new_pair(object_new(NUMBER, &num1), NULL); */
-/*     p1->u.pair->right = p2; */
-/*     p2->u.pair->right = p1; */
-/*     s1->value = p1; */
-/*     s2->value = p2; */
+/*     printf("test_mark :"); */
+/*     reset_mem(); */
+/*     print_free_objs(); */
+/*     int n1 = 12; */
+/*     int n2 = 5; */
+/*     object_t *num1 = object_new(NUMBER, &n1); */
+/*     object_t *sym = object_new(SYMBOL, "A"); */
+/*     object_t *num2 = object_new(NUMBER, &n2); */
+/*     object_t *p3 = new_pair(num2, NULL); */
+/*     object_t *p2 = new_pair(sym, p3); */
+/*     mobject = new_pair(num1, p2); */
+/*     mark_object(mobject); */
+/*     ASSERT(mobject->mark, 1); */
+/*     ASSERT(num1->mark, 1); */
+/*     ASSERT(p2->mark, 1); */
+/*     ASSERT(sym->mark, 1); */
+/*     ASSERT(p3->mark, 1); */
+/*     ASSERT(num2->mark, 1); */
+/* } */
+
+/* /\** */
+/*  * Используем помеченные объекты с предыдущего теста. */
+/*  * Проверить снятие пометок */
+/*  * Проверить, что помеченные ранее объекты не находятся в списке свободных */
+/*  *\/ */
+/* void test_sweep() */
+/* { */
+/*     printf("test_sweep: "); */
+/*     print_free_objs(); */
+/*     object_t *num1 = mobject->u.pair->left; */
+/*     object_t *p2 = mobject->u.pair->right; */
+/*     object_t *sym = p2->u.pair->left; */
+/*     object_t *p3 = p2->u.pair->right; */
+/*     object_t *num2 = p3->u.pair->left; */
+/*     sweep(); */
+/*     printf("after_sweep: "); */
+/*     print_free_objs(); */
+/*     ASSERT(mobject->mark, 0); */
+/*     ASSERT(num1->mark, 0); */
+/*     ASSERT(p2->mark, 0); */
+/*     ASSERT(sym->mark, 0); */
+/*     ASSERT(p3->mark, 0); */
+/*     ASSERT(num2->mark, 0);  */
+/*     object_t *f = free_objs; */
+/*     while (f != NULL) { */
+/* 	//	printf("f type =%d\n", f->type); */
+/* 	if (f == mobject || f == num1 || f == p2 || f == sym || f == p3 || f == num2) { */
+/* 	    printf("fail_object\n"); */
+/* 	    return; */
+/* 	} */
+/* 	f = f->next; */
+/*     } */
+/*     printf("objects: OK\n"); */
+/* } */
+
+/* /\** */
+/*  * Создать символ A */
+/*  * Присвоить ему значение - объект 52 */
+/*  * Проверить, что объект не в списке свободных */
+/*  *\/ */
+/* void test_garbage_collect() */
+/* { */
+/*     printf("test_garbage_collect: "); */
+/*     int num1 = 52; */
+/*     symbol_t *s = new_symbol("A"); */
+/*     object_t *obj1 = object_new(NUMBER, &num1); */
+/*     s->value = obj1; */
 /*     garbage_collect(); */
 
-/*     ASSERT(p1->free, 0); */
-/*     ASSERT(p2->free, 0); */
+/*     object_t *f = free_objs; */
+/*     while (f != NULL) { */
+/* 	if (f == obj1) { */
+/* 	    printf("fail_object\n"); */
+/* 	    return; */
+/* 	} */
+/* 	f = f->next; */
+/*     } */
+/*     printf("OK\n"); */
 /* } */
-	
+
+/* /\** */
+/*  * Создать символ B */
+/*  * Присвоить ему значение - объект (1 2) */
+/*  * Проверить, что объект не в списке свободных */
+/*  *\/ */
+/* void test_garbage_collect_list() */
+/* { */
+/*     printf("test_garbage_collect_list: "); */
+/*     int num1 = 1; */
+/*     int num2 = 2; */
+/*     symbol_t *s = new_symbol("B"); */
+/*     object_t *obj1 = object_new(NUMBER, &num1); */
+/*     object_t *obj2 = object_new(NUMBER, &num2); */
+/*     object_t *p1 = new_pair(obj1, new_pair(obj2, NULL)); */
+/*     s->value = p1; */
+/*     printf("before gc: \n"); */
+/*     printf("objects: "); */
+/*     print_free_objs(); */
+/*     printf("\npairs: "); */
+/*     print_free_pairs(); */
+/*     garbage_collect(); */
+/*     printf("after gc: "); */
+/*     print_free_objs(); */
+
+/*     object_t *f = free_objs; */
+/*     while (f != NULL) { */
+/* 	if (f == obj1 || f == obj2) { */
+/* 	    printf("fail_object\n"); */
+/* 	    return; */
+/* 	} */
+/* 	f = f->next; */
+/*     } */
+/*     printf("objects_OK\n"); */
+/*     pair_t *p = free_pairs; */
+/*     while (p != NULL) { */
+/* 	if (p == p1->u.pair || p == p1->u.pair->right->u.pair) { */
+/* 	    printf("fail_pair\n"); */
+/* 	    return; */
+/* 	} */
+/* 	p = p->next; */
+/*     }  */
+/*     printf("pairs: OK\n"); */
+/* } */
+
 /* /\** */
 /*  * Выделить 2 региона с размерами 5 и 64 */
 /*  * Проверить указатели регионов относительно начала */
@@ -561,14 +449,14 @@ void test_free_region()
 /*     ASSERT(pair, pair); */
 /* } */
 
-/* /\** */
-/*  * Освободить пустую пару */
-/*  *\/ */
-/* void test_free_pair_empty() */
-/* { */
-/*     printf("test_free_pair_empty: "); */
-/*     free_pair(NULL);     */
-/* } */
+/**
+ * Освободить пустую пару
+ */
+void test_free_pair_empty()
+{
+    printf("test_free_pair_empty: ");
+    free_pair(NULL);
+}
 
 /* /\** */
 /*  * Проверка корректности функции освобождения строки */
@@ -626,6 +514,15 @@ void test_free_region()
 /* 	return new_pair(o, NULL); */
 /*     return new_pair(o, make_list(num - 1)); */
 /* } */
+
+/**
+ * Создать массив 
+ */
+void test_new_array()
+{
+    printf("test_new_array: ");
+    //PRINT(make_list(5));
+}
 
 /* /\** */
 /*  * Создать символ B */
@@ -846,9 +743,8 @@ void main()
     test_return_get_addr();
     test_new_object(NUMBER, (void *)10);
     test_new_object(STRING, "test");
-    test_new_bignumber(1100);
-    test_new_bignumber(0);
-    test_new_bignumber(-1520);
+    test_new_bignumber(2000000000);
+    test_new_bignumber(-2000000000);
     test_free_bignumber();
     test_new_number(-677);
     test_new_number(56);
@@ -858,6 +754,9 @@ void main()
     test_get_value(13);
     test_get_value(0);
     test_get_value(-6);
+    test_get_value(86000000);
+    test_get_value(-86000000);
     test_new_pair();
     test_free_pair();
+    test_new_symbol();
 }
