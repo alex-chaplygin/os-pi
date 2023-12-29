8cb1d5d82cd8332fac8adcac57e1bb6d52ac67b8
Чаплыгин Александр <alex_chaplygin@mail.ru>
2023-12-29 19:39:54 +0300

    Переписаны макросы доступа к спискам

--- portable/lisp/eval.h
+++ portable/lisp/eval.h
@@ -1,14 +1,14 @@
 // Первый элемент списка
-#define FIRST(o) o->u.pair->left
+#define FIRST(o) (GET_PAIR(o)->left)
 
 // Второй элемент списка
-#define SECOND(o) o->u.pair->right->u.pair->left
+#define SECOND(o) (GET_PAIR(GET_PAIR(o)->right)->left)
 
 // Третий элемент списка
-#define THIRD(o) o->u.pair->right->u.pair->right->u.pair->left
+#define THIRD(o) (GET_PAIR(GET_PAIR(GET_PAIR(o)->right)->right)->left)
 
 // Хвост списка
-#define TAIL(o) o->u.pair->right
+#define TAIL(o) (GET_PAIR(o)->right)
 
 object_t *eval(object_t *obj, object_t *env);
 void init_eval();
