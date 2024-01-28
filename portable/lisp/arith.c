f9cc033a1c3848d7728cf85337affbd87132dc0e
Чаплыгин Александр <alex_chaplygin@mail.ru>
2024-01-28 18:31:04 +0300

    Модификация арифметических функций

--- portable/lisp/arith.c
+++ portable/lisp/arith.c
@@ -35,55 +35,53 @@ object_t add(object_t list)
     return new_number(num);
 }
 
-/** 
- * Вычитание аргументов (- 1 2 3)
- *
- * @param list - список чисел (1 2 3)
- *
- * @return разность
- */
-/*object_t *sub(object_t *list)
+//Вычитание аргументов (- 1 2 3)
+
+//@param list - список чисел (1 2 3)
+
+//@return разность
+object_t sub(object_t list)
 {
-    if (list == NULL) {
+   
+    if (list == NULLOBJ) {
         error("sub: no arguments\n");
         return ERROR;
     }
-    object_t *first = FIRST(list);
-    int num = first->u.value;
+    object_t first = FIRST(list);
+    int num = get_value(FIRST(list));  
     list = TAIL(list);
-    while (list != NULL) {
-        object_t *first = FIRST(list);
-        if (first->type == NUMBER) {
-            num -= first->u.value;
-            list = TAIL(list);
-        } else {
+    while (list != NULLOBJ) {
+        object_t first = FIRST(list);
+        if  (TYPE(first) == NUMBER || TYPE(first) == BIGNUMBER) {  
+	        num -= get_value(first);
+	        list = TAIL(list);
+	}
+	else {
             error("sub: Not number\n");
             return ERROR;
         }
     }
-    return object_new(NUMBER, &num);
+    return new_number(num);
 }
 
-/** 
- * Умножение аргументов (* 1 2 3)
- *
- * @param list - список чисел (1 2 3)
- *
- * @return произведение
- */
-/*object_t *mul(object_t *list)
+//Умножение аргументов (* 1 2 3)
+
+//@param list - список чисел (1 2 3)
+
+//@return произведение
+object_t mul(object_t list)
 {
-    if (list == NULL) {
+    if (list == NULLOBJ) {
 	error("mul: no arguments\n");
 	return ERROR;
     }
-    object_t *first = FIRST(list);
-    int num = first->u.value;
+    object_t first = FIRST(list);
+    int num = get_value(FIRST(list));
     list = TAIL(list);
-    while(list != NULL){
-	object_t *first = FIRST(list);
-	if(first->type == NUMBER){
-	    num *= first->u.value;
+    while(list != NULLOBJ){
+	object_t first = FIRST(list);
+	if(TYPE(first) == NUMBER || TYPE(first) == BIGNUMBER){
+	    num *= get_value(first);
 	    list = TAIL(list);
 	}
 	else{
@@ -91,7 +89,7 @@ object_t add(object_t list)
 	    return ERROR;
 	}
     }
-    return object_new(NUMBER, &num);
+    return new_number(num);
 }
 
 /**
@@ -101,76 +99,80 @@ object_t add(object_t list)
  *
  * @return результат от деления
  */
+object_t int_div(object_t list){
 
-/*object_t *int_div(object_t *list)
-{
-    if (list == NULL) {
+    if (list == NULLOBJ) {
         error("div: no arguments\n");
         return ERROR;
     }
-    if (TAIL(list) == NULL) {
+    if (TAIL(list) == NULLOBJ) {
         error("div: no divisor\n");
         return ERROR;
     }
-    object_t *first = FIRST(list);
-    object_t *second = SECOND(list);
-    if (second->u.value != 0) {
-        int num = first->u.value / second->u.value;
-        return object_new(NUMBER, &num);
+    object_t first = FIRST(list);
+    if (TYPE(first) != NUMBER && TYPE(first) != BIGNUMBER){
+	 error("mul: Not number\n");
+	 return ERROR;
+    }
+    object_t second = SECOND(list);
+    if (TYPE(second) != NUMBER && TYPE(second) != BIGNUMBER){
+	 error("mul: Not number\n");
+	 return ERROR;
+    }
+    if (get_value(second) != 0) {
+        int num = get_value(first) / get_value(second);
+        return new_number(num);
     } else {
         error("div: divisor = 0 \n");
         return ERROR;
     }
 }
 
-/**
- * Сравнение на больше (> 8 2)
- * 
- * @param list - список чисел (8 2)
- *
- * @return результат от сравнения
- */
-/*object_t *gt(object_t *list)
+/* Сравнение на больше (> 8 2) */
+
+/* @param list - список чисел (8 2) */
+
+/* @return результат от сравнения */
+
+object_t gt(object_t list)
 {
-    if (list == NULL) {
+    if (list == NULLOBJ) {
         error(">: no arguments\n");
         return ERROR;
     }
-    if (TAIL(list) == NULL) {
+    if (TAIL(list) == NULLOBJ) {
         error(">: one argument\n");
         return ERROR;
     }
-    object_t *first = FIRST(list);
-    object_t *second = SECOND(list);
-    if (first->u.value > second->u.value)
+    object_t first = FIRST(list);
+    object_t second = SECOND(list);
+    if (get_value(first) > get_value(second))
         return t;
     else 
-        return NULL;
+        return NULLOBJ;
 }
 
-/**
- * Сравнение на меньше (< 8 2)
- * 
- * @param list - список чисел (8 2)
- *
- * @return результат от сравнения
- */
-/*object_t *less(object_t *list)
+/* Сравнение на меньше (< 8 2) */
+
+/* @param list - список чисел (8 2) */
+
+/* @return результат от сравнения */
+object_t less(object_t list)
 {
-    if (list == NULL) {
+    if (list == NULLOBJ) {
         error("<: no arguments\n");
         return ERROR;
     }
-    if (TAIL(list) == NULL) {
+    if (TAIL(list) == NULLOBJ) {
         error("<: one argument\n");
         return ERROR;
     }
-    object_t *first = FIRST(list);
-    object_t *second = SECOND(list);
-    if (first->u.value < second->u.value)
+    object_t first = FIRST(list);
+    object_t second = SECOND(list);
+    if ( get_value(first) <  get_value(second))
         return t;
     else 
-        return NULL;
+        return NULLOBJ;
 }
 
 /** 
@@ -179,21 +181,8 @@ object_t add(object_t list)
  * @param list - список чисел (1 2)
  *
  * @return T - если равно, иначе NIL
- */
 /*object_t *num_eq(object_t *list)
 {
-    if (list == NULL) {
-        error("num_eq: no arguments\n");
-        return ERROR;
-    }
-    if (TAIL(list) == NULL) {
-        error("num_eq: no second param\n");
-        return ERROR;
-    }
-    if (FIRST(list)->type != NUMBER || SECOND(list)->type != NUMBER) {
-        error("num_eq: not a number\n");
-        return ERROR;
-    }
     object_t *n1 = FIRST(list);
     object_t *n2 = SECOND(list);
     if (n1->u.value == n2->u.value)
@@ -202,10 +191,10 @@ object_t add(object_t list)
 	return NULL;
 }
 
+
 /**
  * Функция сравнения объектов
  * возвращает 1 если значения объектов равны, иначе 0
- */
 /*int compare_obj(object_t *obj1, object_t *obj2)
 {
     if (obj1 == NULL && obj2 == NULL)
@@ -268,22 +257,22 @@ object_t add(object_t list)
  *
  * @return побитовое И
  */
-/*object_t *bitwise_and(object_t *list)
+object_t bitwise_and(object_t list)
 {
-    if (list == NULL) {
+    if (list == NULLOBJ) {
 	error("bitwise_and: no arguments\n");
 	return ERROR;
     }
-    if (FIRST(list)->type != NUMBER) {
+    if (TYPE(FIRST(list)) != NUMBER) {
 	error("bitwise_and: Not a number\n");
 	return ERROR;
     }
-    int num = FIRST(list)->u.value;
+    int num = get_value(FIRST(list));
     list = TAIL(list);
-    while (list != NULL) {
-	object_t *first = FIRST(list);
-	if (first->type == NUMBER) {  
-	    num &= first->u.value;
+    while (list != NULLOBJ) {
+	object_t first = FIRST(list);
+	if (TYPE(first) == NUMBER) {  
+	    num &= get_value(first);
 	    list = TAIL(list);
 	}
 	else {
@@ -291,7 +280,7 @@ object_t add(object_t list)
 	    return ERROR;
 	}
     }
-    return object_new(NUMBER, &num);
+    return new_number(num);
 }
 
 /** 
@@ -301,18 +290,18 @@ object_t add(object_t list)
  *
  * @return побитовое ИЛИ
  */
-/*object_t *bitwise_or(object_t *list)
+object_t bitwise_or(object_t list)
 {
-    if (list == NULL) {
+    if (list == NULLOBJ) {
 	error("bitwise_or: no arguments\n");
 	return ERROR;
     }
-    int num = FIRST(list)->u.value;
+    int num = get_value(FIRST(list));
     list = TAIL(list);
-    while (list != NULL) {
-	object_t *first = FIRST(list);
-	if (first->type == NUMBER) {  
-	    num |= first->u.value;
+    while (list != NULLOBJ) {
+	object_t first = FIRST(list);
+	if (TYPE(first) == NUMBER) {  
+	    num |= get_value(first);
 	    list = TAIL(list);
 	}
 	else {
@@ -320,7 +309,7 @@ object_t add(object_t list)
 	    return ERROR;
 	}
     }
-    return object_new(NUMBER, &num);
+    return new_number(num);
 }
 
 /**
@@ -329,7 +318,6 @@ object_t add(object_t list)
  * @param list - список (<Любое число> <Число бит>)
  *
  * @return результат сдвига
- */
 
 /*object_t *shift_left(object_t *list)
 {
--- portable/lisp/test/test_arith.c
+++ portable/lisp/test/test_arith.c
@@ -11,17 +11,17 @@ extern object_t *t;
 extern object_t *nil;
 
 object_t add(object_t list);
-/*object_t *sub(object_t *list);
-object_t *mul(object_t *list);
-object_t *int_div(object_t *list);
-object_t *num_eq(object_t *list);
-object_t *bitwise_and(object_t *list);
-object_t *bitwise_or(object_t *list);
-object_t *shift_left(object_t *list);
+object_t sub(object_t list);
+object_t mul(object_t list);
+object_t int_div(object_t list);
+/*object_t *num_eq(object_t *list);*/
+object_t bitwise_and(object_t list);
+object_t bitwise_or(object_t list);
+/*object_t *shift_left(object_t *list);
 object_t *shift_right(object_t *list);
 object_t *equal(object_t *list);
-object_t *less(object_t *list);
-object_t *gt(object_t *list);*/
+object_t *less(object_t *list);*/
+object_t gt(object_t list);
 
 void error(char *str, ...)
 {
@@ -72,90 +72,81 @@ void test_add_no_number()
 /**
  * Тест вычитания 
  */
-/*void test_sub()
+void test_sub()
 {
     printf("test_sub: ");
     int num1 = 10;
     int num2 = 3;
     int num3 = 2;
-    object_t *list = new_pair(object_new(NUMBER, &num1),
-                        new_pair(object_new(NUMBER, &num2), 
-                            new_pair(object_new(NUMBER, &num3), NULL)));
-    object_t *res = sub(list);
-    ASSERT(res->u.value, 5);
+    object_t list = new_pair(new_number(num1),
+                        new_pair(new_number(num2), 
+                            new_pair(new_number(num3), NULLOBJ)));
+    object_t res = sub(list);
+    ASSERT(get_value(res),5);
 }
 
-/**
- * Тест вычитания - проверка на NULL.
- */
-/*void test_sub_null()
+//Тест вычитания - проверка на NULL.
+void test_sub_null()
 {
     printf("test_sub_null: ");
-    object_t *list = NULL;
-    object_t *res = sub(list);
+    object_t list = NULLOBJ;
+    object_t res = sub(list);
     ASSERT(res, ERROR);
 }
 
-/**
- * Тест вычитания передача значения не число
- */
-/*void test_sub_no_number()
+//Тест вычитания передача значения не число
+void test_sub_no_number()
 {
     printf("test_sub_no_number: ");
 
     int num = 2;
-    object_t *list = new_pair(object_new(NUMBER, &num),
-			      new_pair(new_pair(NULL, NULL), NULL));
-    object_t *res = sub(list);
+    object_t list = new_pair(new_number(num),
+			      new_pair(new_pair(NULLOBJ, NULLOBJ), NULLOBJ));
+    object_t res = sub(list);
     ASSERT(res, ERROR);
 }
 
-/**
- * Тест умножения.
- */
-/*void test_mul()
+//Тест умножения.
+void test_mul()
 {
     printf ("test_mul:");
     int num1 = 1;
     int num2 = 2;
     int num3 = 3;
 
-    object_t *list = new_pair(object_new(NUMBER, &num1),
-			      new_pair(object_new(NUMBER, &num2),
-				       new_pair(object_new(NUMBER, &num3), NULL)));
+    object_t list = new_pair(new_number(num1),
+			      new_pair(new_number(num2),
+				       new_pair(new_number(num3), NULLOBJ)));
     
-    object_t *res = mul(list);
-    ASSERT(res->u.value, 6); 
+    object_t res = mul(list);
+    ASSERT(get_value(res), 6); 
 }
 
-/**
- * Тест умножения, передача пустого списка.
- */
-/*void test_mul_empty_list()
+
+//Тест умножения, передача пустого списка.
+void test_mul_empty_list()
 {
     printf ("test_mul_empty_list:");
-    object_t *empty_list = NULL;
-    object_t *res = mul(empty_list);
+    object_t empty_list = NULLOBJ;
+    object_t res = mul(empty_list);
     ASSERT(res, ERROR);
 }
 
-/**
- * Тест умножения, передача списка содержащего символ.
- */
-/*void test_mul_list_with_symbol()
+
+// Тест умножения, передача списка содержащего символ.
+void test_mul_list_with_symbol()
 {
     printf ("test_mul_list_with_symbol :");
     int num = 1;
-    object_t *list_with_symbol = new_pair(object_new(NUMBER, &num),
-                                         new_pair(object_new(SYMBOL, "A"), NULL));
-    object_t *res = mul(list_with_symbol);
+    object_t list_with_symbol = new_pair(new_number(num),
+                                         new_pair(NEW_SYMBOL("a"), NULLOBJ));
+    object_t res = mul(list_with_symbol);
     ASSERT(res, ERROR);
 }
 
 /**
  * Тест сравнения чисел
- */
-/*void test_num_eq(int num1, int num2, object_t *token)
+ void test_num_eq(int num1, int num2, object_t *token)
 {
 
     printf ("test_num_eq:");
@@ -166,90 +157,90 @@ void test_add_no_number()
     object_t *res = num_eq(list);
     ASSERT(res, token);   
 }
+*/
 
-/**
- * Тест деления
- */
-/*void test_div()
+ //Тест деления
+void test_div()
 {
     printf("test_div: \n");
     int num1 = 8;
     int num2 = 2;
-    object_t *list = new_pair(object_new(NUMBER, &num1),
-                        new_pair(object_new(NUMBER, &num2), NULL));
-    object_t *res = int_div(list);
-    ASSERT(res->u.value, 4);
+    object_t list = new_pair(new_number(num1),
+                        new_pair(new_number(num2), NULLOBJ));
+    object_t res = int_div(list);
+    ASSERT(get_value(res), 4); 
 }
 
 
 /**
  * Тест пустого списка деления
  */
-/*void test_div_nulllist()
+void test_div_nulllist()
 {
     printf("test_div_nulllist: \n");
-    object_t *list = NULL;
-    object_t *res = int_div(list);
+    object_t list = NULLOBJ;
+    object_t res = int_div(list);
     ASSERT(res,ERROR);
 }
 
 /**
  * Тест нулевого делителя
  */
-/*void test_div_zerodivisor()
+
+void test_div_zerodivisor()
 {
     printf("test_div_zerodivisor: \n");
     int num1 = 8;
     int num2 = 0;
-    object_t *list =  new_pair(object_new(NUMBER,&num1),
-			      new_pair(object_new(NUMBER,&num2),NULL));
-    object_t *res =  int_div(list);
+    object_t list =  new_pair(new_number(num1),
+			      new_pair(new_number(num2),NULLOBJ));
+    object_t res =  int_div(list);
     ASSERT(res,ERROR);
 }
 
-/**
- * Тест пустого делителя
- */
-/*void test_div_nulldivisor()
+
+//Тест пустого делителя
+ 
+void test_div_nulldivisor()
 {
     printf("test_div_nulldivisor: \n");
     int num1 = 8;
-    object_t *list = new_pair(object_new(NUMBER, &num1), NULL);
-    object_t *res =  int_div(list);
+    object_t list = new_pair(new_number(num1), NULLOBJ);
+    object_t res =  int_div(list);
     ASSERT(res,ERROR);
 }
 
 /**
  * Тест побитового И
  */
-/*void test_bitwise_and(int num1, int num2, int res)
+void test_bitwise_and(int num1, int num2, int res)
 {
     printf("test_bitwise_and: %d %d", num1, num2);
-    object_t *list = new_pair(object_new(NUMBER, &num1),
-			      new_pair(object_new(NUMBER, &num2), NULL));
-    object_t *obj_res = bitwise_and(list);
-    ASSERT(obj_res->u.value, res);
+    object_t list = new_pair(new_number(num1),
+			     new_pair(new_number( num2), NULLOBJ));
+    object_t obj_res = bitwise_and(list);
+    ASSERT(get_value(obj_res), res);
 }
 
 /**
 * Тест проверка на NULL.
 */
-/*void test_bitwise_and_null()
+void test_bitwise_and_null()
 {
     printf("test_bitwise_and_null: ");
-    object_t *list = NULL;
-    object_t *res = bitwise_and(list);
+    object_t list = NULLOBJ;
+    object_t res = bitwise_and(list);
     ASSERT(res, ERROR);
 }
 
 /**1
  * Тест передача значения не число
  */
-/*void test_bitwise_and_no_number()
+void test_bitwise_and_no_number()
 {
     printf("test_bitwise_and_no_number: ");
-    object_t *list = new_pair(object_new(SYMBOL, "G"), NULL);
-    object_t *res = bitwise_and(list);
+    object_t list = new_pair(NEW_SYMBOL("G"), NULLOBJ);
+    object_t res = bitwise_and(list);
     ASSERT(res, ERROR);
 }
 
@@ -257,45 +248,45 @@ void test_bitwise_and_number_sym()
 {
     printf("test_bitwise_and_number_sym: ");
     int num1 = 2;
-    object_t *list = new_pair(object_new(NUMBER, &num1),
-			      new_pair(object_new(SYMBOL, "C"), NULL));
-    object_t *res = bitwise_and(list);
+    object_t list = new_pair(new_number(num1),
+			     new_pair(NEW_SYMBOL("C"), NULLOBJ));
+    object_t res = bitwise_and(list);
     ASSERT(res, ERROR);
 }
 
 /**
  * Тест побитового ИЛИ
  */
-/*void test_bitwise_or(int num1, int num2, int res)
+void test_bitwise_or(int num1, int num2, int res)
 {
     printf("test_bitwise_or: %d %d", num1, num2);
-    object_t *list = new_pair(object_new(NUMBER, &num1),
-			      new_pair(object_new(NUMBER, &num2), NULL));
-    object_t *obj_res = bitwise_or(list);
-    ASSERT(obj_res->u.value, res);
+    object_t list = new_pair(new_number(num1),
+			     new_pair(new_number(num2), NULLOBJ));
+    object_t obj_res = bitwise_or(list);
+    ASSERT(get_value(obj_res), res);
 }
 
 /**
  * Тест побитового ИЛИ для пустого ввода
  */
-/*void test_bitwise_or_null()
+void test_bitwise_or_null()
 {
     printf("test_bitwise_or_null:");
-    object_t *list = NULL;
-    object_t *obj_res = bitwise_or(list);
+    object_t list = NULLOBJ;
+    object_t obj_res = bitwise_or(list);
     ASSERT(obj_res, ERROR);
 }
 
 /**
  * Тест побитового ИЛИ для неверного ввода
  */
-/*void test_bitwise_or_no_number()
+void test_bitwise_or_no_number()
 {
     printf("test_bitwise_or_no_number:");
     int num1 = 8;
-    object_t *list = new_pair(object_new(NUMBER, &num1),
-			      new_pair(object_new(SYMBOL, "T"), NULL));
-    object_t *obj_res = bitwise_or(list);
+    object_t list = new_pair(new_number(num1),
+			     new_pair(NEW_SYMBOL("T"), NULLOBJ));
+    object_t obj_res = bitwise_or(list);
     ASSERT(obj_res, ERROR);
 }
 
@@ -560,90 +551,84 @@ void test_bitwise_and_number_sym()
     ASSERT(res, t);
 }
 
-/**
- * Тест сравнения неравнества меньше для двух чисел
- */
-/*void test_less()
-{
-    printf("test_less: ");
-    int first1 = 1;
-    int second1 = 2;
-    object_t *list = new_pair(object_new(NUMBER, &first1),
-			      new_pair(object_new(NUMBER, &second1), NULL));
-    object_t *res = less(list);
-    ASSERT(res, t);
-}
+/* /\** */
+/*  * Тест сравнения неравнества меньше для двух чисел */
+/*  *\/ */
+/* void test_less() */
+/* { */
+/*     printf("test_less: "); */
+/*     int first1 = 1; */
+/*     int second1 = 2; */
+/*     object_t list = new_pair(new_number(first1), */
+/* 			      new_pair(new_number(second1), NULLOBJ)); */
+/*     object_t res = less(list); */
+/*     ASSERT(res, t); */
+/* } */
 
-/**
- * Тест сравнения неравнества меньше для двух чисел - проверка если первое число будет больше
- */
-/*void test_less_great()
-{
-    printf("test_less_great: ");
-    int first1 = 2;
-    int second1 = 1;
-    object_t *list = new_pair(object_new(NUMBER, &first1),
-			      new_pair(object_new(NUMBER, &second1), NULL));
-    object_t *res = less(list);
-    ASSERT(res, NULL);
-}
+/* /\** */
+/*  * Тест сравнения неравнества меньше для двух чисел - проверка если первое число будет больше */
+/*  *\/ */
+/* void test_less_great() */
+/* { */
+/*     printf("test_less_great: "); */
+/*     int first1 = 2; */
+/*     int second1 = 1; */
+/*     object_t list = new_pair(new_number(first1), */
+/* 			     new_pair(new_number(second1), NULLOBJ)); */
+/*     object_t res = less(list); */
+/*     ASSERT(res, NULLOBJ); */
+/* } */
 
-/**
- * Тест сравнения неравнества меньше для двух чисел - проверка на отсутствие агрументов
- */
-/*void test_less_no_arguments()
-{
-    printf("test_less_no_arguments: ");
-    object_t *list = NULL;
-    object_t *res = less(list);
-    ASSERT(res, ERROR);
-}
+/* /\** */
+/*  * Тест сравнения неравнества меньше для двух чисел - проверка на отсутствие агрументов */
+/*  *\/ */
+/* void test_less_no_arguments() */
+/* { */
+/*     printf("test_less_no_arguments: "); */
+/*     object_t list = NULLOBJ; */
+/*     object_t res = less(list); */
+/*     ASSERT(res, ERROR); */
+/* } */
 
-/**
- * Тест сравнения неравнества меньше для двух чисел - проверка на только один аргумент
- */
-/*void test_less_one_argument()
-{
-    printf("test_less_one_argument: ");
-    int first1 = 1;
-    object_t *list = new_pair(object_new(NUMBER, &first1), NULL);
-    object_t *res = less(list);
-    ASSERT(res, ERROR);
-}
+/* /\** */
+/*  * Тест сравнения неравнества меньше для двух чисел - проверка на только один аргумент */
+/*  *\/ */
+/* void test_less_one_argument() */
+/* { */
+/*     printf("test_less_one_argument: "); */
+/*     int first1 = 1; */
+/*     object_t list = new_pair(new_number(first1), NULLOBJ); */
+/*     object_t res = less(list); */
+/*     ASSERT(res, ERROR); */
+/* } */
 
-/**
- * Тест сравнения чисел на больше
- */
-/*void test_gt(int num1, int num2, object_t *token) 
+/*Тест сравнения чисел на больше*/
+void test_gt(int num1, int num2, object_t token) 
 {
     printf("test_gt:");
-    object_t *list = new_pair(object_new(NUMBER, &num1), 
-                       new_pair(object_new(NUMBER, &num2), NULL));
-    object_t *res = gt(list);
+    object_t list = new_pair(new_number(num1), 
+			     new_pair(new_number(num2), NULLOBJ));
+    object_t res = gt(list);
     ASSERT(res, token);
 }
 
-/**
- * Тест сравнения чисел на больше при пустом входном списке 
- */
-/*void test_gt_list_is_null()
+/*Тест сравнения чисел на больше при пустом входном списке*/
+void test_gt_list_is_null()
 {
     printf("test_gt_list_is_null:");
-    object_t *list = NULL;
-    object_t *res = gt(list);
+    object_t list = NULLOBJ;
+    object_t res = gt(list);
     ASSERT(res, ERROR);
 }
 
-/**
- * Тест сравнения чисел на больше при одном аргументе в списке 
- */
-/*void test_gt_one_arg(int num1)
+//Тест сравнения чисел на больше при одном аргументе в списке 
+void test_gt_one_arg(int num1)
 {
     printf("test_gt_one_arg:");
-    object_t *list = new_pair(object_new(NUMBER, &num1), NULL);
-    object_t *res = gt(list);
+    object_t list = new_pair(new_number(num1), NULLOBJ);
+    object_t res = gt(list);
     ASSERT(res, ERROR);
-}*/
+}
 
 int main()
 {
@@ -653,7 +638,7 @@ int main()
     test_add();
     test_add_null();
     test_add_no_number();
-    /*test_sub();
+    test_sub();
     test_sub_null();
     test_sub_no_number();
     test_mul();
@@ -663,8 +648,8 @@ int main()
     test_div_nulllist();
     test_div_zerodivisor();
     test_div_nulldivisor();
-    test_num_eq(1, 2, nil);
-    test_num_eq(10, 10, t);
+    /*test_num_eq(1, 2, nil);
+      test_num_eq(10, 10, t);*/
     test_bitwise_and(0xA, 2, 2);
     test_bitwise_and(0xB, 2, 2);
     test_bitwise_and(0xA, 5, 0);
@@ -675,7 +660,8 @@ int main()
     test_bitwise_or(0, 0, 0);
     test_bitwise_or_null();
     test_bitwise_or_no_number();
-    test_shift_left(1, 2, 4); //100
+
+    /*test_shift_left(1, 2, 4); //100
     test_shift_left(2, 3, 16); //10000
     test_shift_left_empty_list();
     test_shift_left_no_second_param();
@@ -700,10 +686,10 @@ int main()
     test_less();
     test_less_great();
     test_less_no_arguments();
-    test_less_one_argument();
+    test_less_one_argument();*/
     test_gt(5, 3, t);
-    test_gt(3, 5, nil);
+    test_gt(3, 5, NULLOBJ);
     test_gt_list_is_null();
-    test_gt_one_arg(3);*/
+    test_gt_one_arg(3);
     return 0;
 }
