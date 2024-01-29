38c05fee55cee76467dde4ade0479fbc3dc0eeed
Чаплыгин Александр <alex_chaplygin@mail.ru>
2024-01-29 18:46:45 +0300

    Сложение чисел

--- portable/lisp/mpa.c
+++ portable/lisp/mpa.c
@@ -13,10 +13,10 @@ Write your code in this editor and press "Run" button to compile and execute it.
 /// Структура вещественного числа, запсисанного в виде символов
 typedef struct bign
 {
-    char size; // количество цифр в числе
-    char sign; // знак числа
-    char* data; //указатель на массив цифр числа
-    char exponent; // позиция плавающей точки, начиная справа
+    int size; // количество цифр в числе
+    int sign; // знак числа
+    char *data; //указатель на массив цифр числа от младшей к старшей 
+    int exponent; // позиция плавающей точки, начиная справа
 } *bignum_t;
 
 /**
@@ -26,13 +26,14 @@ typedef struct bign
  *
  * @return struct bignum_t bignum
  */
-bignum_t new_bignum(char size)
+bignum_t new_bignum(int size)
 {
-    bignum_t bignum = (bignum_t) malloc(sizeof(struct bign));
+    bignum_t bignum = (bignum_t)malloc(sizeof(struct bign));
     bignum->size = size;
     bignum->sign = 0;
-    bignum->data = (char* )malloc(size * sizeof(char));
-    for(int i = 0; i < size; i++) bignum->data[i] = 0;
+    bignum->data = (char *)malloc(size);
+    for(int i = 0; i < size; i++) 
+        bignum->data[i] = 0;
     bignum->exponent = 0;
     return bignum;
 }
@@ -45,7 +46,7 @@ bignum_t new_bignum(char size)
  * @param Входное число val - значение цифры
  *
  */
-void set_digit(bignum_t bignum, char pos, char val)
+void set_digit(bignum_t bignum, int pos, char val)
 {
     bignum->data[pos] = val;
 }
@@ -57,7 +58,7 @@ void set_digit(bignum_t bignum, char pos, char val)
  * @param Входное число exponent - позиция плавающей точки
  *
  */
-void set_exp(bignum_t bignum, char exponent)
+void set_exp(bignum_t bignum, int exponent)
 {
     bignum->exponent = exponent;
 }
@@ -69,7 +70,7 @@ void set_exp(bignum_t bignum, char exponent)
  * @param Входное число sign - знак числа
  *
  */
-void set_sign(bignum_t bignum, char sign)
+void set_sign(bignum_t bignum, int sign)
 {
     bignum->sign = sign;
 }
@@ -82,67 +83,102 @@ void set_sign(bignum_t bignum, char sign)
  */
 void print_num(bignum_t bignum)
 {
-    if (bignum->sign == -1) putchar('-');
-    for(int i = 0; i < bignum->size - bignum->exponent; i++) printf("%d", bignum->data[i]);
-    if (bignum->exponent != 0) putchar('.');
-    for(int i = bignum->size - bignum->exponent; i < bignum->size; i++) printf("%d", bignum->data[i]);
+    if (bignum->sign == 1) 
+        putchar('-');
+    /*for(int i = 0; i < bignum->size - bignum->exponent; i++) 
+        if (bignum->data[i]!=-1)
+            printf("%d", bignum->data[i]);
+    if (bignum->exponent != 0) 
+        putchar('.');
+    for(int i = bignum->size - bignum->exponent; i < bignum->size; i++)
+        if (bignum->data[i]!=-1)
+            printf("%d", bignum->data[i]); 
+    printf("\n");*/
+    int i = bignum->size - 1;
+    while (bignum->data[i] == 0 && i >= 0)
+        i--;
+    if (i == -1)
+        printf("0");
+    else
+        while (i >= 0) {
+            printf("%d", bignum->data[i]);
+            i--;
+        }
+}
+
+
+
+/**
+ * @brief конвертирует число int в большое число
+ * 
+ * @param n - большое число 
+ * @param num -целое число 
+ */
+void bignum_from_int(bignum_t n, int num)
+{
+    if (num < 0) {
+         n->sign = 1;
+         num = -num;
+    }
+    for(int i = 0; i < n->size; i++) {   
+        n->data[i] = num % 10;
+        num = num / 10;
+        if (num == 0)
+            break;
+    }
 }
 
 /**
- * @brief Печать вещественного числа (Старая версия)
- *  sign(1) exp(8) mant(23)
+ * @brief перезаписывает 1-е большое число, прибавляя к нему 2-е
  * 
- * @param n Входное число
+ * @param n1 - большое 1-е число 
+ * @param n2 - большое 2-е число 
  */
-void print_float_num(float num)
+int bignum_sum(bignum_t n1, bignum_t n2) 
 {
-    int n = *(int *)&num;
-    int s = n >> 31; // знак
-    int e = ((n >> 23) & 0xff) - 127; // порядок
-    int m = n & 0x7fffff; // мантисса
-    int num_bits = 23 - e; // число используемых (значащих) бит в мантиссе
-    printf("%d\n",num_bits);
-    int int_part = (m + (1 << 23)) >> num_bits; // целая часть числа
-    int float_part = (m + (1 << 23)) & (1 << num_bits) - 1; // вещественная часть в степенях 2
-    //printf("%d\n",float_part);
-    long long float_res = 0; // полученная вещественная часть * 10^num_bits
-    long long power2 = 5;
-    int n1=0;
-    printf("S = %d E = %d num = %d, float = %x\n", s, e, int_part, float_part);
-    while (float_res < 1000000000000000000 && float_part != 0) {
-        printf("power2 = %lld n = %d %lld\n", power2, float_part, float_res);
-        if(float_res==0) n1++;
-	    float_res += power2 * ((float_part >> (num_bits - 1)) & 1);
-	    float_res *= 10;
-	    power2 = power2 * 5;
-	    float_part &= (1 << num_bits - 1) - 1;
-	    //printf("%d\n",(1 << num_bits - 1) - 1);
-	    num_bits--;
+    if (n1->size < n2->size) {
+        printf("Args error");
+        return -1;
     }
-    printf("float_res = %lld\n", float_res);
-    if (s == -1){
-        printf("-");
+    int carry = 0;
+    for (int i = 0; i < n2->size; i++) {
+        int sum = n1->data[i] + n2->data[i] + carry;
+        n1->data[i] = sum % 10;
+        carry = sum >= 10;
     }
-    
-    printf("%d.%lld", int_part, float_res);
-    printf("\n%d",n1);
-    
+    if (carry) {
+        printf("Индекс за пределами массива!\n");
+        return -1;
+    }
+    return 0;
 }
+
 int main()
 {
     //float a = 1.0000012345f;
-    bignum_t n = new_bignum(20); // {sign: -1, data:[1,0,0,1,2], exp:2}
-    set_digit(n, 1, 9);
+    bignum_t n1 = new_bignum(40);
+    bignum_t n2 = new_bignum(4);
+    //bignum_t n3 = new_bignum(20);
+   /* set_digit(n, 1, 9);
     set_digit(n, 4, 1);
     set_digit(n, 17, 7);
     set_exp(n, 12);
-    set_sign(n, -1);
+    set_sign(n, -1);*/
+    bignum_from_int(n1, 15);
+    bignum_from_int(n2, 2865);
     //print_float_num(a);
     //printf("\n%f", a);
-    printf("%d %d %d\n",n->sign, n->size, n->exponent);
+    //printf("%d %d %d\n",n->sign, n->size, n->exponent);
     //for(int i=0; i<n.size; i++) printf("%d ", n.data[i]);
-    print_num(n);
-    
+    print_num(n1);
+    printf("\n");
+    print_num(n2);
+    printf("\n");
+    if (bignum_sum(n1, n2) == 0) {
+        printf("Сумма:");
+        print_num(n1);
+    }
+    //print_num(n3);
 
     return 0;
 }
