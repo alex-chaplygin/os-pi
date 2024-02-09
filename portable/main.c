afce584149d770282bf83329bc5b2ab9c30d2cdd
Чаплыгин Александр <alex_chaplygin@mail.ru>
2024-02-09 09:57:25 +0300

    В главную программу добавлена установка точки возврата из ошибки

--- portable/main.c
+++ portable/main.c
@@ -8,38 +8,45 @@
  * 
  */
 
-#include <x86/console.h>
-#include <portable/libc.h>
-#include <portable/mem.h>
-#include <x86/idt.h>
-#include <portable/timer.h>
-#include <x86/irq.h>
-#include <portable/keyboard.h>
-#include <x86/mouse.h>
-#include <x86/gdt.h>
-#include <portable/proc.h>
-#include <x86/cmos.h>
-#include <portable/device.h>
-#include <portable/file.h>
-#include <portable/syslib.h>
-#include <x86/disk.h>
-#include <objects.h>
+#include <x86/x86.h>
 #include <lexer.h>
+#include <objects.h>
 #include <parser.h>
 #include <eval.h>
-#include <arith.h>
-#include <str.h>
-#include <symbols.h>
-#include <array.h>
+#include <portable/libc.h>
+#include <x86/console.h>
+#include <x86/gdt.h>
+#include <x86/idt.h>
+#include <portable/keyboard.h>
+#include <portable/syslib.h>
 
 /// Адрес начала секции .lisp
 const void *_lisp_start;
+/// состояние стека
+jmp_buf jmp_env;
 
 /// последний прочитанный токен
 extern token_t token;
 void graph_init();
 
 void init_all();
+
+/** 
+ * Вывод сообщения об ошибке и выход из программы
+ *
+ * @param str сообщение об ошибке
+ */
+void error(char *str, ...)
+{
+    if (str[0] != '\0') {
+        va_list vals;
+        va_start(vals, str);
+        vprintf(str, vals);
+        va_end(vals);
+        putchar('\n');
+    }
+    longjmp(jmp_env, 1);
+}
 
 /** 
  * Загрузка начального кода lisp
@@ -51,13 +58,15 @@ void boot_lisp()
     boot_code = (char *)&_lisp_start;
     printf("boot = %x\n", &_lisp_start);
     do {
-	object_t *o = parse();
-	if (o != ERROR) {
+	if (setjmp(jmp_env) == 0) {
+	    object_t *o = parse();
+	    if (o == NOVALUE)
+		longjmp(jmp_env, 1);
+	    //printf("parse: "); PRINT(o);
 	    object_t *res = eval(o, NULL);
+	    //printf("res: "); PRINT(res);
 	    print_counter++;
-	    if (res != ERROR)
-		PRINT(res);
-	    garbage_collect();
+	    PRINT(res);
 	}
     } while (token.type != END);
     boot_load = 0;
@@ -80,17 +89,17 @@ void kmain(void)
     graph_init();
     boot_lisp();
     while(1) {
-	printf("> ");
-	object_t *o = parse();
-	//printf("parse: ");
-	//print_obj(o);
-	if (o != ERROR) {
+	if (setjmp(jmp_env) == 0) {
+	    printf("> ");
+	    object_t *o = parse();
+	    if (o == NOVALUE)
+		longjmp(jmp_env, 1);
+	    //printf("parse: "); PRINT(o);
 	    object_t *res = eval(o, NULL);
-	    //printf("\n");
+	    //printf("res: "); PRINT(res);
 	    print_counter++;
-	    if (res != ERROR)
-		PRINT(res);
-	}      
+	    PRINT(res);
+	}
 	garbage_collect();
     }
 }
