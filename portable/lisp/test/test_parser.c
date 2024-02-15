99a97eb149ef8bb1cc07baf7cfbc3d2f87503bc3
Чаплыгин Александр <alex_chaplygin@mail.ru>
2024-02-15 14:46:26 +0300

    Тест - список без правой скобки внутри списка

--- portable/lisp/test/test_parser.c
+++ portable/lisp/test/test_parser.c
@@ -415,8 +415,12 @@ void test_parse_no_rparen_lists()
     count = 0; 
     cur_token = &token; 
     tokens = no_rparen_tokens_lists; 
-    object_t o = parse(); 
-    ASSERT(o, ERROR); 
+
+    if (setjmp(jmp_env) == 0) {
+        object_t o = parse();
+        FAIL;
+    } else
+        OK;
 } 
 
 /**
@@ -742,10 +746,10 @@ int main()
     test_parse_quote(comma_tokens, "COMMA"); //17
     test_parse_list_quote(); //16
     test_parse_no_rparen();  //6
-    //test_parse_no_rparen_lists(); //10
+    test_parse_no_rparen_lists(); //10
     //test_parse_no_rparen_arrays(); //11 23
-      test_parse_inner_list(); //7
-      test_parse_invalid();    //2
+    test_parse_inner_list(); //7
+    //test_parse_invalid();    //2
       /*  test_parse_invalid_quote(); //2
       test_parse_array(); //18 
       test_parse_array_list(); //9
