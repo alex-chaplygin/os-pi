3e41eb3c42e141cddd048f877882ae3fc0b46b21
Чаплыгин Александр <alex_chaplygin@mail.ru>
2024-04-16 08:08:26 +0300

    Правильная инициализация

--- portable/init.c
+++ portable/init.c
@@ -1,9 +1,10 @@
 void init_all()
 {
+    init_regions();
+    init_objects();
     init_eval();
     init_arith();
     init_strings();
-    init_regions();
     init_arrays();
     init_pair();
     init_predicates();
