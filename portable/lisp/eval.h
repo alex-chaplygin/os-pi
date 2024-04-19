bcfac1972178ee64f26b3a02b4a78e0e50501491
Чаплыгин Александр <alex_chaplygin@mail.ru>
2024-04-19 11:20:17 +0300

    Удалены повторяющиеся макросы

--- portable/lisp/eval.h
+++ portable/lisp/eval.h
@@ -1,14 +1,2 @@
-// Первый элемент списка
-#define FIRST(o) (GET_PAIR(o)->left)
-
-// Второй элемент списка
-#define SECOND(o) (GET_PAIR(GET_PAIR(o)->right)->left)
-
-// Третий элемент списка
-#define THIRD(o) (GET_PAIR(GET_PAIR(GET_PAIR(o)->right)->right)->left)
-
-// Хвост списка
-#define TAIL(o) (GET_PAIR(o)->right)
-
 object_t eval(object_t obj, object_t env);
 void init_eval();
