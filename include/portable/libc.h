60a071f9ac3f8dcc018ecd61674efad943276473
Чаплыгин Александр <alex_chaplygin@mail.ru>
2024-04-30 15:23:07 +0300

    Добавлены прототипы функций

--- include/portable/libc.h
+++ include/portable/libc.h
@@ -2,6 +2,7 @@
 
 typedef char *va_list;
 #define NULL 0
+#define EOF (-1)
 #define va_start(cur_arg, args) *((char *)(cur_arg = (char *)&args))
 #define va_arg(cur_arg, type) *((type *)(cur_arg += sizeof(type)))
 #define va_end(cur_arg) cur_arg = NULL
@@ -12,3 +13,6 @@ void vprintf(char *format, va_list args);
 void printf(char *str, ...);
 void clear_buffer(byte* buffer, int offset, int count);
 int strcmp(char *str_one, char *str_two);
+void strcpy(char *str1, char *str2);
+int strlen(char *str);
+char toupper(char ch);
