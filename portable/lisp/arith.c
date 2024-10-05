2914611e9537001dcf4be33a4f79a2c67338de73
Чаплыгин Александр <alex_chaplygin@mail.ru>
2024-10-05 07:30:30 +0300

    Умножение и деление с плавающей точкой, остаток от деления.

--- portable/lisp/arith.c
+++ portable/lisp/arith.c
@@ -22,16 +22,14 @@ object_t add_float(object_t list, float sum)
 	error("add: no arguments\n");
     while(list != NULLOBJ) {
 	object_t first = FIRST(list);
-	if (TYPE(first) == FLOAT){
+	if (TYPE(first) == FLOAT)
 	    sum += GET_FLOAT(first)->value;
-	    list = TAIL(list);
-	}
-	else if(IS_NUMBER(first)) {
+	else if(IS_NUMBER(first))
 	    sum += get_value(first);
-	    list = TAIL(list);
-	}
 	else
-	    error("add: Not number\n");    }
+	    error("add: Not number\n");
+	list = TAIL(list);
+    }
     return new_float(sum);
 }
 
@@ -48,54 +46,112 @@ object_t add(object_t list)
     if (list == NULLOBJ)
 	error("add: no arguments\n");
     while (list != NULLOBJ) {
-	    object_t first = FIRST(list);
-	    if (IS_NUMBER(first)) {  
-	        num += get_value(first);
-	        list = TAIL(list);
-	    }
-	    else if(TYPE(first) == FLOAT)
-		return add_float(list, num);
-	    else
-	        error("add: Not number\n");
+	object_t first = FIRST(list);
+	if (IS_NUMBER(first)) {  
+	    num += get_value(first);
+	    list = TAIL(list);
+	}
+	else if(TYPE(first) == FLOAT)
+	    return add_float(list, num);
+	else
+	    error("add: Not number\n");
     }
     return new_number(num);
 }
 
-//Вычитание аргументов (- 1 2 3)
-
-//@param list - список чисел (1 2 3)
-
-//@return разность
-object_t sub(object_t list)
+/** 
+ * Вычитание чисел с плавающей точкой
+ *
+ * @param list - список чисел (2.56 1 0.03)
+ * @param sub - начальная разность
+ *
+ * @return разность
+ */
+object_t sub_float(object_t list, float sub)
 {
-   
+    if(list == NULLOBJ)
+	error("sub: no argements\n");
+    while(list != NULLOBJ) {
+	object_t first = FIRST(list);
+	if (TYPE(first) == FLOAT)
+	    sub -= GET_FLOAT(first)->value;
+	else if(IS_NUMBER(first))
+	    sub -= get_value(first);
+	else
+	    error("sub: Not number\n");
+	list = TAIL(list);
+    }
+    return new_float(sub);
+}
+
+/**
+ * Вычитание аргументов (- 1 2 3)
+ * 
+ * @param list - список чисел (1 2 3)
+ * 
+ * @return разность
+ */
+object_t sub(object_t list)
+{ 
     if (list == NULLOBJ)
         error("sub: no arguments\n");
     object_t first = FIRST(list);
+    if (TYPE(first) == FLOAT) 
+	return sub_float(TAIL(list), GET_FLOAT(first)->value);
     int num = get_value(FIRST(list));  
     list = TAIL(list);
     while (list != NULLOBJ) {
-        object_t first = FIRST(list);
-        if  (IS_NUMBER(first)) {  
-	        num -= get_value(first);
-	        list = TAIL(list);
+	object_t first = FIRST(list);
+	if (IS_NUMBER(first)) {  
+	    num -= get_value(first);
+	    list = TAIL(list);
 	}
+	else if(TYPE(first) == FLOAT)
+	    return sub_float(list, num);
 	else
-            error("sub: Not number\n");
+	    error("sub: Not number\n");
     }
     return new_number(num);
 }
 
-//Умножение аргументов (* 1 2 3)
-
-//@param list - список чисел (1 2 3)
-
-//@return произведение
+/**
+ * Умножение чисел с плавающей точкой
+ * 
+ * @param list - список чисел (1 2.2 3)
+ * @param mul - начальное произведение
+ * 
+ * @return произведение
+ */
+object_t mul_float(object_t list, float mul)
+{
+    if(list == NULLOBJ)
+	error("mul: no arguments\n");
+    while(list != NULLOBJ) {
+	object_t first = FIRST(list);
+	if (TYPE(first) == FLOAT)
+	    mul *= GET_FLOAT(first)->value;
+	else if(IS_NUMBER(first))
+	    mul *= get_value(first);
+	else
+	    error("mul: Not number\n");
+	list = TAIL(list);
+    }
+    return new_float(mul);
+}
+/**
+ * Умножение аргументов (* 1 2 3)
+ * 
+ * @param list - список чисел (1 2 3)
+ * 
+ * @return произведение
+ */
 object_t mul(object_t list)
 {
     if (list == NULLOBJ)
 	error("mul: no arguments\n");
     object_t first = FIRST(list);
+    if (TYPE(first) == FLOAT) 
+	return mul_float(TAIL(list), GET_FLOAT(first)->value);
     int num = get_value(FIRST(list));
     list = TAIL(list);
     while(list != NULLOBJ){
@@ -104,10 +160,37 @@ object_t mul(object_t list)
 	    num *= get_value(first);
 	    list = TAIL(list);
 	}
+	else if(TYPE(first) == FLOAT)
+	    return mul_float(list, num);
 	else
 	    error("mul: Not number\n");
     }
     return new_number(num);
+}
+
+/**
+ * Деление с плавающей точкой
+ * 
+ * @param first - делимое
+ * @param second - делитель
+ *
+ * @return результат от деления
+ */
+object_t div_float(object_t first, object_t second)
+{
+    float f;
+    float s;
+#define GET_F(o, v)\
+    if (TYPE(o) == FLOAT)\
+	v = GET_FLOAT(o)->value;\
+    else if (IS_NUMBER(o))\
+	v = (float)get_value(o);\
+    else\
+	error("div: Not number\n");
+    GET_F(first, f);
+    GET_F(second, s);
+    printf("%f %f\n", f, s);
+    return new_float(f / s);  
 }
 
 /**
@@ -117,23 +200,51 @@ object_t mul(object_t list)
  *
  * @return результат от деления
  */
-object_t int_div(object_t list){
+object_t DIV(object_t list){
 
     if (list == NULLOBJ)
         error("div: no arguments\n");
     if (TAIL(list) == NULLOBJ)
         error("div: no divisor\n");
     object_t first = FIRST(list);
+    object_t second = SECOND(list);
+    if(TYPE(first) == FLOAT || TYPE(second) == FLOAT)
+	return div_float(first, second);
+    if (get_value(second) != 0) {		
+	if (!IS_NUMBER(first))
+	    error("div: Not number\n");
+	if (!IS_NUMBER(second))
+	    error("div: Not number\n");
+	int num = get_value(first) / get_value(second);
+	return new_number(num);
+    } else 
+        error("div: divisor = 0");
+}
+
+/**
+ * Остаток от деления (% 8 2)
+ * 
+ * @param list - список чисел (8 2)
+ *
+ * @return остаток от деления
+ */
+object_t mod(object_t list){
+
+    if (list == NULLOBJ)
+        error("mod: no arguments\n");
+    if (TAIL(list) == NULLOBJ)
+        error("mod: no modisor\n");
+    object_t first = FIRST(list);
     if (!IS_NUMBER(first))
-	 error("mul: Not number\n");
+	 error("mod: Not number\n");
     object_t second = SECOND(list);
     if (!IS_NUMBER(first))
-	 error("mul: Not number\n");
+	 error("mod: Not number\n");
     if (get_value(second) != 0) {
-        int num = get_value(first) / get_value(second);
+        int num = get_value(first) % get_value(second);
         return new_number(num);
     } else 
-        error("div: divisor = 0 \n");
+        error("mod: divisor = 0 \n");
 }
 
 /* Сравнение на больше (> 8 2) */
@@ -331,7 +442,8 @@ void init_arith()
     register_func("-", sub);
     register_func("*", mul);
     register_func("EQUAL", equal);
-    register_func("/", int_div);
+    register_func("/", DIV);
+    register_func("%", mod);
     register_func("&", bitwise_and);
     register_func("BITOR", bitwise_or);
     register_func("<<", shift_left);
