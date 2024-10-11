2e6e43b9a5c570d1bca0477344c62e83c2e3d91b
Чаплыгин Александр <alex_chaplygin@mail.ru>
2024-10-11 07:37:10 +0300

    Макрос для проверки чисел с плавающей точкой

--- portable/lisp/test/test.h
+++ portable/lisp/test/test.h
@@ -4,5 +4,15 @@
     else {\
 	printf("%d != %d fail\n", (int)v1, (int)v2);	\
     }
+#define EPSILON 0.000001f
+#define ABS(v) ((v) < 0 ? -(v) : v)
+#define ASSERT_FLOAT(f1, f2) \
+    if (ABS(f1 - f2) < EPSILON) \
+    printf("%f OK \n", f2); \
+    else {\
+    printf("%f != %f fail \n", f1, f2); \
+    }
+
+
 #define OK ASSERT(0,0)
 #define FAIL ASSERT(0,1)
