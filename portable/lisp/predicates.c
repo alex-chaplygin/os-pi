4f83238336b2308470edade9a3e845aad36b8706
Чаплыгин Александр <alex_chaplygin@mail.ru>
2024-10-19 14:17:02 +0300

    Предикат для целых чисел

--- portable/lisp/predicates.c
+++ portable/lisp/predicates.c
@@ -9,23 +9,35 @@
  */
 object_t symbolp(object_t params)
 {   
-    if (params == NULLOBJ) {
+    if (params == NULLOBJ) 
 	error("SYMBOLP: no params");
-	return ERROR;
-    } else if (TAIL(params) != NULLOBJ) {
+    else if (TAIL(params) != NULLOBJ) 
 	error("SYMBOLP: many params");
-	return ERROR;
-    }
-    object_t symbol = FIRST(params);
-    
+    object_t symbol = FIRST(params);    
     if (TYPE(symbol) == SYMBOL)
-	return NEW_OBJECT(SYMBOL, find_symbol("T"));
+	return t;
     else
-	return NULLOBJ;
+	return nil;
 }
 
+/*
+ * Функция integerp, которая возвращает T, если obj - целое число и NIL, если нет
+ */
+object_t integerp(object_t params)
+{   
+    if (params == NULLOBJ) 
+	error("INTEGERP: no params");
+     else if (TAIL(params) != NULLOBJ) 
+	error("INTEGERP: many params");
+    object_t integer = FIRST(params);
+    if (TYPE(integer) == NUMBER || TYPE(integer) == BIGNUMBER)
+	return t;
+    else
+	return nil;
+}
 
 void init_predicates()
 {
     register_func("SYMBOLP", symbolp);
+    register_func("INTEGERP", integerp);	
 }
