19c25c5fa804099a9669c89bd560e06685ebddf8
Чаплыгин Александр <alex_chaplygin@mail.ru>
2024-10-26 13:59:56 +0300

    Документация

--- include/portable/libc.h
+++ include/portable/libc.h
@@ -1,10 +1,14 @@
 #include <portable/types.h>
 
-typedef char *va_list;
+typedef char *va_list;		/**< Тип - строка из параметров */
 #define NULL 0
-#define EOF (-1)
+#define EOF (-1)		/**< Конец потока */
+#define MANTISSA_BITS 23	/**< Число бит в мантиссе float */
+/// Инициализация параметров
 #define va_start(cur_arg, args) *((char *)(cur_arg = (char *)&args))
+/// Извлечение параметра
 #define va_arg(cur_arg, type) *((type *)(cur_arg += sizeof(type)))
+/// Конец обработки параметров
 #define va_end(cur_arg) cur_arg = NULL
 
 void memcpy(void* destptr, const void* srcptr, unsigned int size);
