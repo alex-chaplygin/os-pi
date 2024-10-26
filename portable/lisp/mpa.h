b44bb3e4646719797ab49cfe56be06c51e0d05c8
Чаплыгин Александр <alex_chaplygin@mail.ru>
2024-10-26 14:01:41 +0300

    Новое поле next в структуре

--- portable/lisp/mpa.h
+++ portable/lisp/mpa.h
@@ -1,10 +1,6 @@
 #ifndef BIGNUM_H
 #define BIGNUM_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
 #define MAX_BIGNUM_SIZE 100
 #define MAX_BIGNUMS 100
 
@@ -15,18 +11,18 @@ typedef struct bign
     int sign; /// Знак числа
     char data[MAX_BIGNUM_SIZE]; /// Цифры числа, начиная с младшей цифры
     int exponent;
+    struct bign *next;
+    int free;
 } *bignum_t;
 
 bignum_t new_bignum();
 bignum_t new_bignum_from_str(const char* str);
-void set_digit(bignum_t bignum, int pos, char val);
-void set_exp(bignum_t bignum, int exponent);
-void set_sign(bignum_t bignum, int sign);
-void print_num(bignum_t bignum);
+void free_bignum(bignum_t o);
 void print_bignum(bignum_t num);
-void bignum_from_int(bignum_t n, int num);
 int bignum_sum(bignum_t n1, bignum_t n2);
 int bignum_sub(bignum_t n1, bignum_t n2);
 int bignum_mult(bignum_t n1, bignum_t n2);
+int bignum_compare(bignum_t n1, bignum_t n2);
+int bignum_div(bignum_t n1, bignum_t n2);
 
 #endif 
