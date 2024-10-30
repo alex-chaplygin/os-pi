a53d3aebbdca0de2f0f48772a6f72fa5a02d476a
Чаплыгин Александр <alex_chaplygin@mail.ru>
2024-10-30 09:03:44 +0300

    Тесты для вычитания

--- portable/lisp/test/test_mpa.c
+++ portable/lisp/test/test_mpa.c
@@ -18,6 +18,21 @@ void test_bignum_sum(const char* num1, const char* num2, const char* expected_re
     }
 }
 
+void test_bignum_sub(const char* num1, const char* num2, const char* expected_result, int r)
+{
+    printf("test_bignum_sub: %s %s\n", num1, num2);
+    bignum_t bignum1 = new_bignum_from_str(num1);
+    bignum_t bignum2 = new_bignum_from_str(num2);
+    bignum_t result_bignum = new_bignum_from_str(expected_result);
+
+    int res = bignum_sub(bignum1, bignum2);
+    ASSERT(res, r);
+    if (r >= 0) {
+	ASSERT(bignum1->size, result_bignum->size);
+	ASSERT(memcmp(bignum1->data, result_bignum->data, bignum1->size * sizeof(bignum1->data[0])), 0);
+    }
+}
+
 void test_bignum_mult(const char* num1, const char* num2, const char* expected_result)
 {
     printf("test_bignum_mult: %s %s\n", num1, num2);
@@ -80,6 +95,19 @@ int main()
     test_bignum_sum("20", "10z", "", -2);
     test_bignum_sum("501", "503", "1004", 0);    
 
+    test_bignum_sub("10", "10", "0", 0);
+    test_bignum_sub("20", "-10", "30", 0);
+    test_bignum_sub("503", "501", "2", 0);
+    test_bignum_sub("0", "0", "0", 0);
+    test_bignum_sub("0", "1", "-1", 0);
+    test_bignum_sub("0", "-1", "1", 0);
+    test_bignum_sub("-1", "0", "-1", 0);
+    test_bignum_sub("-1", "1", "-2", 0);
+    test_bignum_sub("-1", "-1", "0", 0);
+    test_bignum_sub("1", "0", "1", 0);
+    test_bignum_sub("1", "1", "0", 0);
+    test_bignum_sub("1", "-1", "2", 0);
+ 
     test_bignum_mult("732", "841", "615612");
     test_bignum_mult("25", "2", "50");
     test_bignum_mult("5486", "1475", "8091850");
