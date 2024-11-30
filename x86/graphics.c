45a1d8696be5c2b26fc0be57c9259946e7071fbb
Чаплыгин Александр <alex_chaplygin@mail.ru>
2024-11-30 17:13:59 +0300

    Копирование части текстового буфера в видеопамять

--- x86/graphics.c
+++ x86/graphics.c
@@ -5,6 +5,10 @@
 #include <portable/libc.h>
 #include <parser.h>
 
+#define TEXT_BUF_WIDTH 160 // число байт в строке текстового буфера
+#define TEXT_BUF_HEIGHT 25 // число строк в текстовом буфере
+#define TEXT_VIDEO_MEM 0xB8000 // Адрес текстовой видео памяти 
+
 /**
  * Отправляет массив в буфер видеопамяти
  * 
@@ -23,7 +27,38 @@ object_t graph_send_buffer(object_t params)
     return NULLOBJ;
 }
 
+/**
+ * Отправляет часть экрана в буфер текстовой видеопамяти
+ * 
+ * @param params - (буфер, координаты x,y копируемой области, ширина и высота копируемой области )
+ */
+object_t send_text_buffer(object_t params)
+{
+    array_t *buf = GET_ARRAY(FIRST(params));
+    int x = get_value(SECOND(params));
+    int y = get_value(THIRD(params));
+    int w = 2 * get_value(FIRST(TAIL(TAIL(TAIL(params)))));
+    int h = get_value(SECOND(TAIL(TAIL(TAIL(params)))));
+    byte b[TEXT_BUF_WIDTH] = {0x30, 0x33, 0x31, 0x34};
+    byte *bp;
+
+    int start_offset = TEXT_BUF_WIDTH * y + x * 2;
+    byte *dst = (byte *)TEXT_VIDEO_MEM + start_offset;
+    object_t *src = buf->data + start_offset;
+    for (int yi = 0; yi < h; yi++) {
+	bp = b;
+	for (int i = 0; i < w; i++)
+	    *bp++ = GET_CHAR(src[i]);
+	memcpy(dst, b, w);
+	dst += TEXT_BUF_WIDTH;
+	src += TEXT_BUF_WIDTH;
+    }
+}
+
 void graph_init()
 {
     register_func("GRAPH-SEND-BUFFER", graph_send_buffer);
+    register_func("SEND-TEXT-BUFFER", send_text_buffer);
+    
 }
+
