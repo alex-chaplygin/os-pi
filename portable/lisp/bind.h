b3fcb7041dc6aa55ed1c927b3a2979561109cc16
Чаплыгин Александр <alex_chaplygin@mail.ru>
2024-12-13 19:08:31 +0300

    Связывание и развязывание временных объектов

--- portable/lisp/bind.h
+++ portable/lisp/bind.h
@@ -7,14 +7,31 @@ typedef struct bind_s
     struct bind_s *next; //Указатель на следующий элемент в списке
 } bind_t;
 
+/// Добавить один объект в список защиты protected
+#define PROTECT1(o)\
+   bind_t p_obj;\
+   p_obj.obj = o;\
+   p_obj.next = protected;\
+   bind_t *old_ped = protected;\
+   protected = &p_obj;
+
+/// Добавить 2 объекта в список защиты protected
+#define PROTECT2(o1, o2)\
+   bind_t p_obj1;\
+   bind_t p_obj2;\
+   p_obj1.obj = o1;\
+   p_obj1.next = &p_obj2;\
+   p_obj2.obj = o2;\
+   p_obj2.next = protected;\				
+   bind_t *old_ped = protected;\
+   protected = &p_obj1;
+
+
+#define UNPROTECT\
+   protected = old_ped;
+
 extern bind_t *global_env;
-
-/// Добавить одну переменную в список защиты
-#define PROTECT1(var)
-
-#define UNPROTECT
+extern bind_t* protected;
 
 void bind_global(object_t symbol);
-
 void set_global(symbol_t *symbol);
-
