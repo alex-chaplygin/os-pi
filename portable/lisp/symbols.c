3cb55340d5bcf05a6f13b7272485e2a6639bb003
Чаплыгин Александр <alex_chaplygin@mail.ru>
2025-01-04 12:51:14 +0300

    Добавлена функция удаления из хеш таблицы

--- portable/lisp/symbols.c
+++ portable/lisp/symbols.c
@@ -3,9 +3,7 @@
 #include <string.h>
 #include "objects.h"
 #include "symbols.h"
-
-#define HASH_SIZE 1000
-#define MAX_SYMBOL_SIZE 80
+#include "bind.h"
 
 symbol_t *hash_table[HASH_SIZE];
 
@@ -15,19 +13,8 @@ unsigned int hash(char *str)
     unsigned int res = 456886;//*str++; 
   
     while (*str)
-        res = (res * 846431 + *str++)%257659;
+        res = (res * 846431 + *str++) % 257659;
     return res;
-}
-
-int compare_str(char *str1, char *str2)
-{
-    while (*str1 && *str2)
-    {
-        if (*str1++ != *str2++)
-        return 0;
-    }
-    
-    return *str1 == *str2;
 }
 
 /** 
@@ -39,14 +26,14 @@ int compare_str(char *str1, char *str2)
  */
 symbol_t *check_symbol(char *str)
 {
-    if (str[0] == '\0' || strlen(str) > MAX_SYMBOL_SIZE)
+    if (str[0] == '\0' || strlen(str) > MAX_SYM_STR)
         return NULL;
     int i = hash(str) % HASH_SIZE;
     symbol_t *el = hash_table[i];
     if (el == NULL)
         return NULL;
     for (symbol_t *cur = el; cur != NULL; cur = cur->next)
-            if (compare_str(cur->str, str))
+            if (!strcmp(cur->str, str))
                 return cur;    
     return NULL;
 }
@@ -60,22 +47,21 @@ symbol_t *check_symbol(char *str)
  */
 symbol_t *find_symbol(char *str) 
 {
-    if (str[0] == '\0' || strlen(str) > MAX_SYMBOL_SIZE)
+    if (str[0] == '\0' || strlen(str) > MAX_SYM_STR)
         return NULL;
     int i = hash(str) % HASH_SIZE;
     symbol_t *el = hash_table[i];
     if (el == NULL ){
         symbol_t *new = new_symbol(str);
         hash_table[i] = new;
+	new->hash_index = i;
         el = new;
-    }
-    else
-    {
+    } else {
         for (symbol_t *cur = el; cur != 0; cur = cur->next)
-            if (compare_str(cur->str, str))
+            if (!strcmp(cur->str, str))
                 return cur;
-                
         symbol_t *new = new_symbol(str);
+	new->hash_index = i;
         symbol_t *last = el;
         while (last->next != 0)
             last = last->next;
@@ -83,6 +69,24 @@ symbol_t *find_symbol(char *str)
         return new;
     }    
     return el;
+}
+
+/** 
+ * удаляет символ из таблицы
+ * @param s - удаляемый символ
+ */
+void hash_remove(symbol_t *s)
+{
+    symbol_t *el = hash_table[s->hash_index];
+    if (el == NULL)
+       error("Hash remove error: got NULL on symbol %s index %d", s->str, s->hash_index);
+    if (el == s)
+        hash_table[s->hash_index] = el->next;
+    else {
+       for (symbol_t *cur = el; cur != NULL; cur = cur->next)
+                   if (cur->next == s)
+               cur->next = s->next;
+    }    
 }
 
 /**
@@ -94,6 +98,7 @@ void register_func(char *name, func_t func_ptr)
 {
     symbol_t *s = find_symbol(name);
     s->func = func_ptr;
+    bind_static(NEW_OBJECT(SYMBOL, s));
 }
 
 void print_table() 
--- portable/lisp/symbols.h
+++ portable/lisp/symbols.h
@@ -1,8 +1,11 @@
 #ifndef SYMBOLS
 #define SYMBOLS
+/// Размер хеш таблицы
+#define HASH_SIZE 1000
 
 symbol_t *check_symbol(char *str);
 symbol_t *find_symbol(char *str);
+void hash_remove(symbol_t *s);
 void register_func(char *name, func_t func_ptr);
 #endif
 
