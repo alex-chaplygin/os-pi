11be0e98ca03ccd1cbb6943a1bf39ea14a28e4af
Чаплыгин Александр <alex_chaplygin@mail.ru>
2025-01-27 12:04:52 +0300

    Преобразование примитивов

--- portable/lisp/symbols.h
+++ portable/lisp/symbols.h
@@ -6,6 +6,6 @@
 symbol_t *check_symbol(char *str);
 symbol_t *find_symbol(char *str);
 void hash_remove(symbol_t *s);
-void register_func(char *name, func_t func_ptr, int nary, int count);
+void register_func(char *name, func0_t func_ptr, int nary, int count);
 #endif
 
