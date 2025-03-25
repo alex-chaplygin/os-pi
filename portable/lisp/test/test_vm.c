b06fd3bfac707d3848038948ddaef9d9efa2a513
Чаплыгин Александр <alex_chaplygin@mail.ru>
2025-03-25 09:46:33 +0300

    Тесты global-set, global-ref

--- portable/lisp/test/test_vm.c
+++ portable/lisp/test/test_vm.c
@@ -33,7 +33,7 @@ enum {
 
 extern object_t *pc_reg;
 extern object_t acc_reg;
-object_t t = 1;
+extern object_t *global_var_memory;
 
 void *alloc_region(int size)
 {
@@ -52,10 +52,11 @@ int get_value(object_t o)
 
 /**
  * Тест инструкции const
+ * Программа с командной CONST. Проверяется значение ACC
  */
 void test_const()
 {
-    printf("test_const_init: ");
+    printf("test_const_init:\n");
     object_t pc_progmem[] = {
 	N(CONST), N(0),
 	N(HALT)
@@ -68,10 +69,11 @@ void test_const()
 
 /**
  * Тест инструцкии jmp
+ * Тестирует безусловный переход. После выполнения команды JMP 5 должен совершиться относительный переход к команде JMP -7, и, соответсвенно, должна выполниться команда CONST 2. Таким образом, значение ACC должно быть равно 2
  */
 void test_jmp()
 {
-    printf("test_jmp: ");
+    printf("test_jmp:\n");
     object_t pc_progmem[] = {
 	N(CONST), N(0),
         N(JMP), N(5),
@@ -82,33 +84,54 @@ void test_jmp()
 	N(HALT)
     };
     object_t pc_constmem[] = {N(1), N(2), N(3)};
-    vm_init(pc_progmem, 16, pc_constmem, 3 ,0);
+    vm_init(pc_progmem, 12, pc_constmem, 3 ,0);
     vm_run();
-    ASSERT(get_value(acc_reg), 2);
+    ASSERT(get_value(acc_reg),2);
 }
 
 /**
  * Тест инструкции jnt
+ * Тестирует переход, если ACC == NIL. В ACC записывается 1, а потом NULLOBJ и проверяется, правильно ли работает условие перехода.
  */
 void test_jnt()
 {
-    printf("test_jnt: ");
+    printf("test_jnt:\n");
     object_t pc_progmem[] = {
-	N(CONST), N(0),
-	N(JNT),	N(5),
 	N(CONST), N(1),
-	N(HALT),
+	N(JNT),	N(0),
+	N(CONST), N(0),
+	N(JNT), N(2),
 	N(CONST), N(2),
-	N(JNT),	N(5),
-	N(CONST), N(3),
-	N(HALT),
-	N(CONST), N(4),
 	N(HALT)
     };
-    object_t pc_constmem[] = {N(1), N(1), N(0), N(2), N(3)};
-    vm_init(pc_progmem,17,pc_constmem, 5, 0);
+    object_t pc_constmem[] = { NULLOBJ, N(1), N(2) };
+    vm_init(pc_progmem, 11, pc_constmem, 3, 0);
+    vm_run(); 
+    ASSERT(get_value(acc_reg), 0);
+}
+
+/**
+ * Тест инструкций GLOBAL-SET и GLOBAL-REF
+ * Устанавливает число 10 в ACC, устанавливает глобальной переменной с индексом 0 значение 10, устанавливает в ACC другое значние и считывает глобальную переменную в регистр ACC. Таким образом, проверяется корректность работы команд GLOBAL-REF и GLOBAL-SET 
+ */
+void test_global_ref_set()
+{
+    printf("test_global_set:\n");
+    object_t pc_progmem[] = {
+	N(CONST), N(0),
+	N(GLOBALSET), N(0),
+	N(CONST), N(1),
+	N(GLOBALREF), N(0),
+	N(HALT)
+    };
+
+    object_t pc_constmem[] = { N(10), N(20) };
+    vm_init(pc_progmem, 5, pc_constmem, 1, 1);
     vm_run();
-    ASSERT(get_value(acc_reg), 2);
+
+    int n = get_value(global_var_memory[0]);
+    ASSERT(n, 10);
+    ASSERT(get_value(acc_reg), 10);
 }
 
 int main()
@@ -116,5 +139,6 @@ int main()
     test_const();
     test_jmp();
     test_jnt();
+    test_global_ref_set();
     return 0;
 }
