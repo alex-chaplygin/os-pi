969f3336031887ecf822154b609235730fa55912
Чаплыгин Александр <alex_chaplygin@mail.ru>
2025-06-03 13:26:48 +0300

    Отдельная функция для вызова формы с переменным числом аргументов

--- portable/lisp/eval.h
+++ portable/lisp/eval.h
@@ -4,6 +4,7 @@ object_t atom(object_t obj1);
 object_t eq(object_t p1, object_t p2);
 object_t error_func(object_t args);
 object_t apply(object_t params);
+object_t call_nary_form(func0_t f, object_t args, int count);
 void init_eval();
 #ifdef DEBUG
 extern object_t debug_stack;
